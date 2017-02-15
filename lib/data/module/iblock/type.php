bd8f417cdeb8d1bbd3b537bfa4f0865b875cb382
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-02-15 17:00:18 +0300

    master: добавлен класс для типов ИБ

---
+++ lib/data/module/iblock/type.php
@@ -0,0 +1,67 @@
+<?namespace Intervolga\Migrato\Data\Module\Iblock;
+
+use Bitrix\Main\Loader;
+use Intervolga\Migrato\Data\BaseData;
+use Intervolga\Migrato\Tool\DataRecord;
+use Intervolga\Migrato\Tool\DataRecordId;
+use Intervolga\Migrato\Tool\XmlIdProviders\TableXmlIdProvider;
+
+class Type extends BaseData
+{
+	protected function __construct()
+	{
+		Loader::includeModule("iblock");
+		$this->xmlIdProvider = new TableXmlIdProvider($this);
+	}
+
+	/**
+	 * @return array|DataRecord[]
+	 */
+	public function getFromDatabase()
+	{
+		$result = array();
+		$getList = \CIBlockType::GetList();
+		while ($type = $getList->fetch())
+		{
+			$record = new DataRecord();
+			$id = DataRecordId::createStringId($type["ID"]);
+			$record->setXmlId($this->getXmlIdProvider()->getXmlId($id));
+			$record->setId($id);
+			$record->setFields(array(
+				"ID" => $type["ID"],
+				"SECTIONS" => $type["SECTIONS"],
+				"EDIT_FILE_BEFORE" => $type["EDIT_FILE_BEFORE"],
+				"EDIT_FILE_AFTER" => $type["EDIT_FILE_AFTER"],
+				"IN_RSS" => $type["IN_RSS"],
+				"SORT" => $type["SORT"],
+			));
+			$result[] = $record;
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param DataRecord $record
+	 */
+	public function update(DataRecord $record)
+	{
+		// TODO: Implement update() method.
+	}
+
+	/**
+	 * @param DataRecord $record
+	 */
+	public function create(DataRecord $record)
+	{
+		// TODO: Implement create() method.
+	}
+
+	/**
+	 * @param $xmlId
+	 */
+	public function delete($xmlId)
+	{
+		// TODO: Implement delete() method.
+	}
+}
