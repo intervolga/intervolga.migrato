2f1108510c84d2d733c0164a8f7c07b127f9631f
Анатолий Ерофеев <erofeev@intervolga.ru>
2017-02-28 11:12:04 +0300

    tools: добавлен скрипт экспорта настроек

---
+++ tools/exportoption.php
@@ -0,0 +1,41 @@
+<?php
+$_SERVER["DOCUMENT_ROOT"] = realpath(dirname(__FILE__)."/../../../..");
+$DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];
+
+define("NO_KEEP_STATISTIC", true);
+define("NOT_CHECK_PERMISSIONS",true);
+define('NO_AGENT_CHECK', true);
+define("STATISTIC_SKIP_ACTIVITY_CHECK", true);
+$isCli = php_sapi_name() == "cli";
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+@set_time_limit(0);
+if (\Bitrix\Main\Loader::includeModule("intervolga.migrato"))
+{
+	try
+	{
+		\Intervolga\Migrato\Tool\Process\ExportOption::run();
+		$report = \Intervolga\Migrato\Tool\Process\ExportOption::getReports();
+	}
+	catch (\Exception $exception)
+	{
+		$report = array(
+			"EXCEPTION (Class: " . get_class($exception) . ")",
+			"Message: " . $exception->getMessage() . " (Code: " . $exception->getCode() . ")",
+			"Location: " . $exception->getFile() . ":" . $exception->getLine()
+		);
+	}
+}
+else
+{
+	$report = array("Module intervolga.migrato not installed");
+}
+if ($isCli)
+{
+	echo implode("\r\n", $report)."\r\n";
+}
+else
+{
+	echo "<pre>" . implode("<br>", $report) . "</pre>";
+}
