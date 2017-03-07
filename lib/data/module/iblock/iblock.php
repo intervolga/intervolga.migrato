2f161ae9c4a5a12a21a02ed28d98d14d2612748d
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-03-07 10:35:44 +0300

    iblock: исправлена ошибка с удалением несуществующего ИБ

--- lib/data/module/iblock/iblock.php
+++ lib/data/module/iblock/iblock.php
@@ -102,10 +102,13 @@ class Iblock extends BaseData
 	public function delete($xmlId)
 	{
 		$id = $this->findRecord($xmlId);
-		$iblockObject = new \CIBlock();
-		if (!$iblockObject->delete($id->getValue()))
+		if ($id)
 		{
-			throw new \Exception("Unknown error");
+			$iblockObject = new \CIBlock();
+			if (!$iblockObject->delete($id->getValue()))
+			{
+				throw new \Exception("Unknown error");
+			}
 		}
 	}
 }
