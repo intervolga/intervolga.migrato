725b89c70ef138726e2518ef1aa5ec3024bb4ba3
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-03-14 12:40:13 +0300

    core: добавлена операция очистки файловой системы от xml файлов записей

---
+++ tools/cleardata.php
@@ -0,0 +1,15 @@
+<?php
+include dirname(__DIR__) . "/include/tools_before.php";
+
+try
+{
+	\Intervolga\Migrato\Tool\Process\ClearData::run();
+	$report = \Intervolga\Migrato\Tool\Process\ClearData::getReports();
+	\Intervolga\Migrato\Tool\Page::showReport($report);
+}
+catch (\Exception $exception)
+{
+	\Intervolga\Migrato\Tool\Page::handleException($exception);
+}
+
+include dirname(__DIR__) . "/include/tools_after.php";
