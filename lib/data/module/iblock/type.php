71492a376e364906cfdf7ed7d499a5bc568bc86a
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-03-27 11:10:06 +0300

    iblock: typelang внесен в type

--- lib/data/module/iblock/type.php
+++ lib/data/module/iblock/type.php
@@ -5,6 +5,7 @@ use Intervolga\Migrato\Data\BaseData;
 use Intervolga\Migrato\Data\Record;
 use Intervolga\Migrato\Data\RecordId;
 use Bitrix\Main\Localization\LanguageTable;
+use Intervolga\Migrato\Data\Value;
 
 class Type extends BaseData
 {
@@ -31,41 +32,108 @@ class Type extends BaseData
 				"IN_RSS" => $type["IN_RSS"],
 				"SORT" => $type["SORT"],
 			));
+			$this->addLanguageStrings($record);
 			$result[] = $record;
 		}
 
 		return $result;
 	}
 
-	protected function getDefaultLanguages()
-    {
-        $languages = array();
-        $rsLanguages = LanguageTable::getList(array(
-            "select" => array(
-                "LID"
-            )
-        ));
-        while ($language = $rsLanguages->fetch())
-        {
-            $languages[$language["LID"]] = array("NAME" => "Unknown");
-        }
-        return $languages;
-    }
+	/**
+	 * @return array|string[]
+	 * @throws \Bitrix\Main\ArgumentException
+	 */
+	protected function getLanguages()
+	{
+		$result = array();
+		$getList = LanguageTable::getList(array(
+			"select" => array(
+				"LID"
+			)
+		));
+		while ($language = $getList->fetch())
+		{
+			$result[] = $language["LID"];
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param \Intervolga\Migrato\Data\Record $record
+	 */
+	protected function addLanguageStrings(Record $record)
+	{
+		$strings = array();
+		foreach ($this->getLanguages() as $language)
+		{
+			if ($typeLang = \CIBlockType::GetByIDLang($record->getId()->getValue(), $language))
+			{
+				foreach ($this->getLanguageFields() as $languageField)
+				{
+					$strings[$languageField][$language] = $typeLang[$languageField];
+				}
+			}
+		}
+		foreach ($strings as $field => $langFields)
+		{
+			$typeLangs = Value::treeToList($langFields, $field);
+			$record->addFieldsRaw($typeLangs);
+		}
+	}
+
+	/**
+	 * @return string[]
+	 */
+	protected function getLanguageFields()
+	{
+		return array(
+			"NAME",
+			"SECTION_NAME",
+			"ELEMENT_NAME"
+		);
+	}
 
 	public function update(Record $record)
 	{
 		$typeObject = new \CIBlockType();
-		$isUpdated = $typeObject->update($record->getId()->getValue(), $record->getFieldsRaw());
+		$fields = $record->getFieldsRaw($this->getLanguageFields());
+		$fields = $this->extractLanguageFields($fields);
+		$isUpdated = $typeObject->update($record->getId()->getValue(), $fields);
 		if (!$isUpdated)
 		{
 			throw new \Exception(trim(strip_tags($typeObject->LAST_ERROR)));
 		}
 	}
 
+	/**
+	 * @param array $fields
+	 *
+	 * @return array
+	 */
+	protected function extractLanguageFields(array $fields)
+	{
+		$typeLang = array();
+		foreach ($this->getLanguageFields() as $field)
+		{
+			foreach ($fields[$field] as $language => $langString)
+			{
+				$typeLang[$language][$field] = $langString;
+			}
+			unset($fields[$field]);
+		}
+		if ($typeLang)
+		{
+			$fields["LANG"] = $typeLang;
+		}
+
+		return $fields;
+	}
+
 	public function create(Record $record)
 	{
-		$fields = $record->getFieldsRaw();
-		$fields["LANG"] = $this->getDefaultLanguages();
+		$fields = $record->getFieldsRaw($this->getLanguageFields());
+		$fields = $this->extractLanguageFields($fields);
 		$typeObject = new \CIBlockType();
 		$typeId = $typeObject->add($fields);
 		if ($typeId)
