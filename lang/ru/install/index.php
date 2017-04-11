4669abdd3673927b727303f72ef60f19b1d58db0
IvanHod <vandamkakdam1@gmail.com>
2017-04-11 10:17:36 +0300

    MIGRATO-26: сохранение таблиц при удалении модуля

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -3,3 +3,4 @@ $MESS["INTERVOLGA_MIGRATO_MODULE_NAME"] = "Интерволга Миграто";
 $MESS["INTERVOLGA_MIGRATO_MODULE_DESC"] = "Миграция БД";
 $MESS["INTERVOLGA_MIGRATO_PARTNER_NAME"] = "ИНТЕРВОЛГА";
 $MESS["INTERVOLGA_MIGRATO_PARTNER_URI"] = "http://www.intervolga.ru";
+$MESS["MIGRATO_UNINSTALL_TITLE"] = "Удаление модуля \"Интерволга Миграто\"";
