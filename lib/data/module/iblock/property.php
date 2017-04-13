05261c2be0887b57bceb7711a60a81df11307808
IvanHod <vandamkakdam1@gmail.com>
2017-04-13 10:42:51 +0300

    MIGRATO-39: реализованы экспорт и импорт опций умного фильтра для свойств элемента

--- lib/data/module/iblock/property.php
+++ lib/data/module/iblock/property.php
@@ -1,6 +1,7 @@
 <?namespace Intervolga\Migrato\Data\Module\Iblock;
 
 use Bitrix\Iblock\PropertyTable;
+use Bitrix\Iblock\SectionPropertyTable;
 use Bitrix\Main\Loader;
 use Intervolga\Migrato\Data\BaseData;
 use Intervolga\Migrato\Data\Record;
@@ -31,26 +32,33 @@ class Property extends BaseData
 			$record = new Record($this);
 			$record->setXmlId($property["XML_ID"]);
 			$record->setId(RecordId::createNumericId($property["ID"]));
-			$record->addFieldsRaw(array(
-				"NAME" => $property["NAME"],
-				"ACTIVE" => $property["ACTIVE"],
-				"SORT" => $property["SORT"],
-				"CODE" => $property["CODE"],
-				"DEFAULT_VALUE" => $property["DEFAULT_VALUE"],
-				"PROPERTY_TYPE" => $property["PROPERTY_TYPE"],
-				"ROW_COUNT" => $property["ROW_COUNT"],
-				"COL_COUNT" => $property["COL_COUNT"],
-				"LIST_TYPE" => $property["LIST_TYPE"],
-				"MULTIPLE" => $property["MULTIPLE"],
-				"FILE_TYPE" => $property["FILE_TYPE"],
-				"MULTIPLE_CNT" => $property["MULTIPLE_CNT"],
-				"WITH_DESCRIPTION" => $property["WITH_DESCRIPTION"],
-				"SEARCHABLE" => $property["SEARCHABLE"],
-				"FILTRABLE" => $property["FILTRABLE"],
-				"IS_REQUIRED" => $property["IS_REQUIRED"],
-				"USER_TYPE" => $property["USER_TYPE"],
-				"HINT" => $property["HINT"],
+
+			$smartFilterOptions = $this->getSmartFilterOptions($property["IBLOCK_ID"], $property["ID"]);
+
+			$record->addFieldsRaw(array_merge(
+				$smartFilterOptions,
+				array (
+					"NAME" => $property["NAME"],
+					"ACTIVE" => $property["ACTIVE"],
+					"SORT" => $property["SORT"],
+					"CODE" => $property["CODE"],
+					"DEFAULT_VALUE" => $property["DEFAULT_VALUE"],
+					"PROPERTY_TYPE" => $property["PROPERTY_TYPE"],
+					"ROW_COUNT" => $property["ROW_COUNT"],
+					"COL_COUNT" => $property["COL_COUNT"],
+					"LIST_TYPE" => $property["LIST_TYPE"],
+					"MULTIPLE" => $property["MULTIPLE"],
+					"FILE_TYPE" => $property["FILE_TYPE"],
+					"MULTIPLE_CNT" => $property["MULTIPLE_CNT"],
+					"WITH_DESCRIPTION" => $property["WITH_DESCRIPTION"],
+					"SEARCHABLE" => $property["SEARCHABLE"],
+					"FILTRABLE" => $property["FILTRABLE"],
+					"IS_REQUIRED" => $property["IS_REQUIRED"],
+					"USER_TYPE" => $property["USER_TYPE"],
+					"HINT" => $property["HINT"],
+					)
 			));
+
 			if ($property["USER_TYPE_SETTINGS"])
 			{
 				if ($userTypeSettings = unserialize($property["USER_TYPE_SETTINGS"]))
@@ -77,6 +85,47 @@ class Property extends BaseData
 		}
 
 		return $result;
+	}
+
+	/**
+	 * @param $iblockId
+	 * @param $propertyId
+	 *
+	 * @return
+	 */
+	private function getSmartFilterOptions($iblockId, $propertyId)
+	{
+		$rsPropertySmartFilter = SectionPropertyTable::getList(array(
+			"filter" => array("IBLOCK_ID" => $iblockId, "PROPERTY_ID" => $propertyId)
+		));
+
+		$properties = array();
+		if($prop = $rsPropertySmartFilter->fetch())
+		{
+			$properties["SECTION_ID"]       = $prop["SECTION_ID"];
+			$properties["SMART_FILTER"]     = $prop["SMART_FILTER"];
+			$properties["DISPLAY_TYPE"]     = $prop["DISPLAY_TYPE"];
+			$properties["DISPLAY_EXPANDED"] = $prop["DISPLAY_EXPANDED"];
+			$properties["FILTER_HINT"]      = $prop["FILTER_HINT"];
+		}
+		return $properties;
+	}
+
+	public function updateSmartFilterOptions($iblockId, $propertyId, $options)
+	{
+		$smartfilterOptions = array(
+			"SMART_FILTER"     => $options["SMART_FILTER"],
+			"DISPLAY_TYPE"     => $options["DISPLAY_TYPE"],
+			"DISPLAY_EXPANDED" => $options["DISPLAY_EXPANDED"],
+			"FILTER_HINT"      => $options["FILTER_HINT"]
+		);
+
+		$id = array("IBLOCK_ID" => $iblockId, "PROPERTY_ID" => $propertyId);
+		if($options["SECTION_ID"])
+		{
+			$id["SECTION_ID"] = intval($options["SECTION_ID"]);
+		}
+		$result = SectionPropertyTable::update($id, $smartfilterOptions);
 	}
 
 	public function getDependencies()
@@ -127,6 +176,7 @@ class Property extends BaseData
 		{
 			throw new \Exception(trim(strip_tags($propertyObject->LAST_ERROR)));
 		}
+		$this->updateSmartFilterOptions($fields["IBLOCK_ID"], $record->getId()->getValue(), $fields);
 	}
 
 	/**
@@ -161,6 +211,7 @@ class Property extends BaseData
 		$propertyId = $propertyObject->add($fields);
 		if ($propertyId)
 		{
+			$this->updateSmartFilterOptions($fields["IBLOCK_ID"], $propertyId, $fields);
 			return RecordId::createNumericId($propertyId);
 		}
 		else
