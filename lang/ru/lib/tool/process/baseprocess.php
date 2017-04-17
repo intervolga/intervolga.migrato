fa3544753e265fe4c28f4484ebfe69a54a4ae683
IvanHod <vandamkakdam1@gmail.com>
2017-04-17 11:53:01 +0300

    MIGRATO-41: добавлены lang зависимости для всех модулей и сущностей

--- lang/ru/lib/tool/process/baseprocess.php
+++ lang/ru/lib/tool/process/baseprocess.php
@@ -1,3 +1,36 @@
 <?
-$MESS["INTERVOLGA_MIGRATO.STATISTICS_RECORD"] = "#MODULE#/#ENTITY# #OPERATION# #COUNT# record(s)";
-$MESS["INTERVOLGA_MIGRATO.STATISTIC_ONE_RECORD"] = "#MODULE#/#ENTITY# #OPERATION# record #DATA_XML_ID#";
+$MESS["INTERVOLGA_MIGRATO.STATISTICS_RECORD"]       = "#MODULE#/#ENTITY# #OPERATION# #COUNT# record(s)";
+$MESS["INTERVOLGA_MIGRATO.STATISTIC_ONE_RECORD"]    = "#MODULE#/#ENTITY# #OPERATION# record #DATA_XML_ID#";
+
+$MESS["INTERVOLGA_MIGRATO.STEP_VALIDATE"]           = "Валидация данных";
+$MESS["INTERVOLGA_MIGRATO.STEP_EXPORT"]             = "Экспорт данных";
+$MESS["INTERVOLGA_MIGRATO.VALIDATE.NOT_MIGRATE"]    = "Сущности, которые НЕ будут мигрироваться:";
+
+$MESS["INTERVOLGA_MIGRATO.MODULE_MAIN"]             = "Главный модуль";
+$MESS["INTERVOLGA_MIGRATO.MODULE_IBLOCK"]           = "Информационные блоки";
+$MESS["INTERVOLGA_MIGRATO.MODULE_HIGHLOADBLOCK"]    = "Highload-блоки";
+$MESS["INTERVOLGA_MIGRATO.MODULE_SALE"]             = "Интернет-магазин";
+$MESS["INTERVOLGA_MIGRATO.MODULE_CATALOG"]          = "Торговый каталог";
+
+$MESS["INTERVOLGA_MIGRATO.ENTITY_GROUP"]            = "Группы пользователей";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_EVENTTYPE"]        = "Типы почтовых событий";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_EVENT"]            = "Шаблоны почтовых событий";
+
+$MESS["INTERVOLGA_MIGRATO.ENTITY_TYPE"]             = "Типы информационных блоков";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_IBLOCK"]           = "Информационные блоки";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_PERMISSION"]       = "Права доступа";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_SECTION"]          = "Группы";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_ELEMENT"]          = "Элементы";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_FIELD"]            = "Пользовательские поля";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_FIELDENUM"]        = "Пользовательские поля (список)";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_PROPERTY"]         = "Свойства";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_ENUM"]             = "Свойства(список)";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_FORM"]             = "Форма отображения страницы редактирования";
+
+$MESS["INTERVOLGA_MIGRATO.ENTITY_HIGHLOADBLOCK"]    = "Highload-блоки";
+
+$MESS["INTERVOLGA_MIGRATO.ENTITY_PRICETYPE"]        = "Типы цен";
+
+$MESS["INTERVOLGA_MIGRATO.ENTITY_PERSONTYPE"]       = "Типы плательщиков";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_PROPERTYGROUP"]    = "Группы свойств заказов";
+$MESS["INTERVOLGA_MIGRATO.ENTITY_PROPERTYVARIANT"]  = "Варианты значений свойств заказов";
