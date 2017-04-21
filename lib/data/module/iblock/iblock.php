ef924b938ce52e252850d8344f735882238c233d
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-04-21 16:39:03 +0300

    MIGRATO-58

--- lib/data/module/iblock/iblock.php
+++ lib/data/module/iblock/iblock.php
@@ -108,10 +108,16 @@ class Iblock extends BaseData
 		}
 	}
 
+	/**
+	 * @param \Intervolga\Migrato\Data\Record $record
+	 *
+	 * @throws \Exception
+	 */
 	protected function addSeoSettings(Record $record)
 	{
 		$seoProps = new IblockTemplates($record->getId()->getValue());
-		if ($templates = $seoProps->findTemplates())
+		$templates = $this->getEmptySeoProperties();
+		if ($templates = array_merge($templates, $seoProps->findTemplates()))
 		{
 			foreach ($templates as $k => $template)
 			{
@@ -121,6 +127,36 @@ class Iblock extends BaseData
 			$record->addFieldsRaw($fieldsValues);
 		}
 	}
+
+	/**
+	 * @return string[]
+	 */
+	protected function getEmptySeoProperties()
+	{
+		return array(
+			"SECTION_META_TITLE" => "",
+			"SECTION_META_KEYWORDS" => "",
+			"SECTION_META_DESCRIPTION" => "",
+			"SECTION_PAGE_TITLE" => "",
+			"ELEMENT_META_TITLE" => "",
+			"ELEMENT_META_KEYWORDS" => "",
+			"ELEMENT_META_DESCRIPTION" => "",
+			"ELEMENT_PAGE_TITLE" => "",
+			"SECTION_PICTURE_FILE_ALT" => "",
+			"SECTION_PICTURE_FILE_TITLE" => "",
+			"SECTION_PICTURE_FILE_NAME" => "",
+			"SECTION_DETAIL_PICTURE_FILE_ALT" => "",
+			"SECTION_DETAIL_PICTURE_FILE_TITLE" => "",
+			"SECTION_DETAIL_PICTURE_FILE_NAME" => "",
+			"ELEMENT_PREVIEW_PICTURE_FILE_ALT" => "",
+			"ELEMENT_PREVIEW_PICTURE_FILE_TITLE" => "",
+			"ELEMENT_PREVIEW_PICTURE_FILE_NAME" => "",
+			"ELEMENT_DETAIL_PICTURE_FILE_ALT" => "",
+			"ELEMENT_DETAIL_PICTURE_FILE_TITLE" => "",
+			"ELEMENT_DETAIL_PICTURE_FILE_NAME" => "",
+		);
+	}
+
 	public function getDependencies()
 	{
 		return array(
@@ -130,7 +166,13 @@ class Iblock extends BaseData
 
 	public function update(Record $record)
 	{
-		$fields = $record->getFieldsRaw();
+		$fields = $record->getFieldsRaw(array("SEO", "FIELDS", "MESSAGES"));
+		$seo = $fields["SEO"];
+		$fieldsSettings = $fields["FIELDS"];
+		$messages = $fields["MESSAGES"];
+		unset($fields["SEO"]);
+		unset($fields["FIELDS"]);
+		unset($fields["MESSAGES"]);
 
 		if($typeId = $this->getDependency("IBLOCK_TYPE_ID")->getId())
 		{
@@ -138,7 +180,13 @@ class Iblock extends BaseData
 		}
 		$iblockObject = new \CIBlock();
 		$isUpdated = $iblockObject->update($record->getId()->getValue(), $fields);
-		if (!$isUpdated)
+		if ($isUpdated)
+		{
+			$this->importMessages($record->getId()->getValue(), $messages);
+			$this->importFields($record->getId()->getValue(), $fieldsSettings);
+			$this->importSeo($record->getId()->getValue(), $seo);
+		}
+		else
 		{
 			throw new \Exception(trim(strip_tags($iblockObject->LAST_ERROR)));
 		}
@@ -146,7 +194,14 @@ class Iblock extends BaseData
 
 	public function create(Record $record)
 	{
-		$fields = $record->getFieldsRaw();
+		$fields = $record->getFieldsRaw(array("SEO", "FIELDS", "MESSAGES"));
+		$seo = $fields["SEO"];
+		$fieldsSettings = $fields["FIELDS"];
+		$messages = $fields["MESSAGES"];
+		unset($fields["SEO"]);
+		unset($fields["FIELDS"]);
+		unset($fields["MESSAGES"]);
+
 		if($iblockTypeId = $record->getDependency("IBLOCK_TYPE_ID")->getId())
 		{
 			$fields["IBLOCK_TYPE_ID"] = $iblockTypeId->getValue();
@@ -155,7 +210,11 @@ class Iblock extends BaseData
 			$iblockId = $iblockObject->add($fields);
 			if ($iblockId)
 			{
-				return $this->createId($iblockId);
+				$id = $this->createId($iblockId);
+				$this->importMessages($iblockId, $messages);
+				$this->importFields($iblockId, $fieldsSettings);
+				$this->importSeo($iblockId, $seo);
+				return $id;
 			}
 			else
 			{
@@ -166,6 +225,36 @@ class Iblock extends BaseData
 		{
 			throw new \Exception("IBlock " . $record->getXmlId() . " haven`t dependency");
 		}
+	}
+
+	/**
+	 * @param int $iblockId
+	 * @param string[] $messages
+	 */
+	protected function importMessages($iblockId, array $messages = array())
+	{
+		$iblock = new \CIBlock();
+		$iblock->setMessages($iblockId, $messages);
+	}
+
+	/**
+	 * @param int $iblockId
+	 * @param array $fields
+	 */
+	protected function importFields($iblockId, array $fields = array())
+	{
+		$iblock = new \CIBlock();
+		$iblock->setFields($iblockId, $fields);
+	}
+
+	/**
+	 * @param int $iblockId
+	 * @param array $seo
+	 */
+	protected function importSeo($iblockId, array $seo = array())
+	{
+		$seoProps = new IblockTemplates($iblockId);
+		$seoProps->set($seo);
 	}
 
 	public function delete($xmlId)
