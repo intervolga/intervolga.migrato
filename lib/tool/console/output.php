20573dfdddde36c6e71b349062be915097695e8c
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-05-17 08:55:06 +0300

    console: добавлены стили вывода (info, ok, fail)

--- lib/tool/console/output.php
+++ lib/tool/console/output.php
@@ -1,11 +1,19 @@
 <?namespace Intervolga\Migrato\Tool\Console;
 
+use Symfony\Component\Console\Formatter\OutputFormatterInterface;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\Output as SymfonyOutput;
 
 class Output extends ConsoleOutput
 {
 	protected $isWindowsCharset = false;
+
+	public function __construct($verbosity = self::VERBOSITY_NORMAL, $decorated = false, OutputFormatterInterface $formatter = null)
+	{
+		parent::__construct($verbosity, $decorated, $formatter);
+		$this->initStyles();
+	}
 
 	public function setWindowsCharset($isWindowsCharset = true)
 	{
@@ -29,5 +37,17 @@ class Output extends ConsoleOutput
 			}
 		}
 		parent::write($messages, $newline, $options);
+	}
+
+	protected function initStyles()
+	{
+		$style = new OutputFormatterStyle('blue', null, array('bold', 'blink'));
+		$this->getFormatter()->setStyle('info', $style);
+
+		$style = new OutputFormatterStyle('green');
+		$this->getFormatter()->setStyle('ok', $style);
+
+		$style = new OutputFormatterStyle('red');
+		$this->getFormatter()->setStyle('fail', $style);
 	}
 }
