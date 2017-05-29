e7a742e8b8d5b56a81c90bcec0ff78040a27c141
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-05-29 11:43:15 +0300

    Добавлена опция quick для импорта

--- lang/ru/lib/tool/console/command/importcommand.php
+++ lang/ru/lib/tool/console/command/importcommand.php
@@ -1,2 +1,3 @@
 <?
 $MESS['INTERVOLGA_MIGRATO.IMPORT_DESCRIPTION'] = 'XML => БД (данные и настройки)';
+$MESS['INTERVOLGA_MIGRATO.NO_AFTER_CLEAR_DESCRIPTION'] = 'Пропустить сброс кэша, поискового индекса и urlrewrite.php';
