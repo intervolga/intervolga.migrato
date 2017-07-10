cce4731e10f314fade80fa5ac9e8339aadf934a4
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-07-10 10:57:35 +0300

    MIGRATO-70: правка сообщения пользователю

--- lang/ru/lib/data/module/iblock/iblock.php
+++ lang/ru/lib/data/module/iblock/iblock.php
@@ -1,2 +1,2 @@
 <?
-$MESS['INTERVOLGA_MIGRATO.IBLOCK_VERSION_NOT_EQUAL'] = 'Измените место хранения инфоблока [#ID#] #NAME# с #VERSION_SITE# на #VERSION_IMPORT#';
+$MESS['INTERVOLGA_MIGRATO.IBLOCK_VERSION_NOT_EQUAL'] = 'Измените версию инфоблока [#ID#] #NAME# вручную на #VERSION_IMPORT#.0';
