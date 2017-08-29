b324e32947d57362126ee8157622a8e6e088fbc5
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-08-29 14:24:07 +0300

    MIGRATO-103: Добавлен код проверки параметров

---
+++ lib/tool/code.php
@@ -0,0 +1,147 @@
+<?php
+namespace Intervolga\Migrato\Tool;
+
+use Bitrix\Main\Application;
+use Bitrix\Main\IO\Directory;
+use Bitrix\Main\IO\File;
+
+class Code
+{
+	/**
+	 * @return \Bitrix\Main\IO\File[]
+	 * @throws \Bitrix\Main\IO\FileNotFoundException
+	 */
+	public static function getTemplateFiles()
+	{
+		$result = array();
+		$root = \Bitrix\Main\Application::getDocumentRoot();
+		/**
+		 * @var \Bitrix\Main\IO\Directory[] $dirs
+		 */
+		$dirs = array(
+			new Directory($root . '/bitrix/templates/'),
+			new Directory($root . '/local/templates/'),
+		);
+		foreach ($dirs as $dir)
+		{
+			foreach ($dir->getChildren() as $templateDir)
+			{
+				if ($templateDir instanceof Directory)
+				{
+					foreach ($templateDir->getChildren() as $templateFile)
+					{
+						$checkFiles = array('header.php', 'footer.php');
+						if (in_array($templateFile->getName(), $checkFiles))
+						{
+							$result[] = $templateFile;
+						}
+					}
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @return \Bitrix\Main\IO\File[]
+	 * @throws \Bitrix\Main\IO\FileNotFoundException
+	 */
+	public static function getPublicFiles()
+	{
+		$root = Application::getDocumentRoot();
+		$dir = new Directory($root);
+		/**
+		 * @var \Bitrix\Main\IO\File[] $check
+		 */
+		$check = array();
+		foreach ($dir->getChildren() as $fileSystemEntry)
+		{
+			if (!static::isServiceEntry($fileSystemEntry))
+			{
+				if ($fileSystemEntry instanceof File)
+				{
+					if (static::isCodeFile($fileSystemEntry))
+					{
+						$check[] = $fileSystemEntry;
+					}
+				}
+				if ($fileSystemEntry instanceof Directory)
+				{
+					$check = array_merge($check, static::getFilesRecursive($fileSystemEntry));
+				}
+			}
+		}
+
+		return $check;
+	}
+
+	/**
+	 * @param \Bitrix\Main\IO\FileSystemEntry $fileSystemEntry
+	 * @return bool
+	 */
+	protected static function isServiceEntry(\Bitrix\Main\IO\FileSystemEntry $fileSystemEntry)
+	{
+		if ($fileSystemEntry->isFile())
+		{
+			if ($fileSystemEntry->getName() == 'urlrewrite.php')
+			{
+				return true;
+			}
+		}
+		if ($fileSystemEntry->isDirectory())
+		{
+			$names = array(
+				'bitrix',
+				'local',
+				'upload',
+				'.git',
+				'.svn',
+			);
+			if (in_array($fileSystemEntry->getName(), $names))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param \Bitrix\Main\IO\File $file
+	 * @return bool
+	 */
+	protected static function isCodeFile(File $file)
+	{
+		return ($file->getExtension() == 'php');
+	}
+
+	/**
+	 * @param Directory $dir
+	 * @return array
+	 * @throws \Bitrix\Main\IO\FileNotFoundException
+	 */
+	protected static function getFilesRecursive(Directory $dir)
+	{
+		$result = array();
+		if ($dir->isExists())
+		{
+			foreach ($dir->getChildren() as $fileSystemEntry)
+			{
+				if ($fileSystemEntry instanceof File)
+				{
+					if (static::isCodeFile($fileSystemEntry))
+					{
+						$result[] = $fileSystemEntry;
+					}
+				}
+				if ($fileSystemEntry instanceof Directory)
+				{
+					$result = array_merge($result, static::getFilesRecursive($fileSystemEntry));
+				}
+			}
+		}
+
+		return $result;
+	}
+}
--- lib/tool/console/command/checkexeccommand.php
+++ lib/tool/console/command/checkexeccommand.php
@@ -1,11 +1,18 @@
-<?namespace Intervolga\Migrato\Tool\Console\Command;
+<?php
+namespace Intervolga\Migrato\Tool\Console\Command;
 
 use Bitrix\Main\Localization\Loc;
+use Intervolga\Migrato\Tool\Code;
+use Intervolga\Migrato\Tool\Console\TableHelper;
 
 Loc::loadMessages(__FILE__);
 
 class CheckExecCommand extends BaseCommand
 {
+	/**
+	 * @var \Intervolga\Migrato\Tool\Console\TableHelper table
+	 */
+	protected $table = null;
 	protected function configure()
 	{
 		$this->setName('checkexec');
@@ -14,6 +21,172 @@ class CheckExecCommand extends BaseCommand
 
 	public function executeInner()
 	{
+		$this->table = new TableHelper();
+		$this->initTable();
+		$errors = static::getComponentErrors();
+		foreach ($errors as $error)
+		{
+			$this->addErrorToTable($error);
+		}
 
+		$this->logger->add($this->table->getOutput());
+	}
+
+	protected function initTable()
+	{
+		$headers = array(
+			'FILE' => Loc::getMessage('INTERVOLGA_MIGRATO.HEADER_FILE'),
+			'LINE' => Loc::getMessage('INTERVOLGA_MIGRATO.HEADER_LINE'),
+			'COMPONENT' => Loc::getMessage('INTERVOLGA_MIGRATO.HEADER_COMPONENT'),
+			'PARAM' => Loc::getMessage('INTERVOLGA_MIGRATO.HEADER_PARAM'),
+			'VALUE' => Loc::getMessage('INTERVOLGA_MIGRATO.HEADER_VALUE'),
+		);
+		$this->table->addHeader($headers);
+	}
+
+	/**
+	 * @param array $error
+	 */
+	protected function addErrorToTable(array $error)
+	{
+		foreach ($error['PARAMS_VALUES'] as $param => $value)
+		{
+			$row = array(
+				'FILE' => $error['FILE'],
+				'LINE' => $error['LINE'],
+				'COMPONENT' => $error['COMPONENT'],
+				'PARAM' => $param,
+				'VALUE' => is_array($value) ? implode(PHP_EOL, $value) : $value,
+			);
+			$this->table->addRow($row);
+		}
+	}
+
+	/**
+	 * @return array
+	 * @throws \Bitrix\Main\IO\FileNotFoundException
+	 */
+	public function getComponentErrors()
+	{
+		$result = array();
+		/**
+		 * @var \Bitrix\Main\IO\File[] $check
+		 */
+		$check = array_merge(Code::getPublicFiles(), Code::getTemplateFiles());
+		foreach ($check as $file)
+		{
+			$data = \PHPParser::parseScript($file->getContents());
+			if ($data)
+			{
+				foreach ($data as $component)
+				{
+					\Bitrix\Main\Diag\Debug::writeToFile(__FILE__ . ':' . __LINE__ . "\n(" . date('Y-m-d H:i:s').")\n" . print_r($component, TRUE) . "\n\n", '', 'log/__debug.log');
+					if ($errors = static::getComponentProbablyNumericIds($component['DATA']))
+					{
+						$result[] = array(
+							'FILE' => $file->getPath(),
+							'LINE' => $component['START'],
+							'COMPONENT' => $component['DATA']['COMPONENT_NAME'],
+							'PARAMS_VALUES' => $errors,
+						);
+					}
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param array $component
+	 * @return array
+	 */
+	protected function getComponentProbablyNumericIds(array $component)
+	{
+		$foo = array();
+		foreach ($component['PARAMS'] as $param => $value)
+		{
+			if (static::isProbablyIdNumeric($param, $value))
+			{
+				$foo[$param] = $value;
+			}
+		}
+
+		return $foo;
+	}
+
+	/**
+	 * @param string $param
+	 * @param mixed $value
+	 * @return bool
+	 */
+	protected function isProbablyIdNumeric($param, $value)
+	{
+		if (static::isProbablyIdParam($param))
+		{
+			if (is_array($value))
+			{
+				if (static::containsNumericValue($value))
+				{
+					return true;
+				}
+			}
+			else
+			{
+				if (static::isNumericValue($value))
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param string $param
+	 * @return bool
+	 */
+	protected function isProbablyIdParam($param)
+	{
+		$probablyIdParams = array(
+			'IBLOCK_ID',
+			'PROPERTY_CODE',
+		);
+		foreach ($probablyIdParams as $probablyIdParam)
+		{
+			if (substr_count($param, $probablyIdParam) > 0)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param array $values
+	 * @return bool
+	 */
+	protected function containsNumericValue(array $values)
+	{
+		foreach ($values as $value)
+		{
+			if (static::isNumericValue($value))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param mixed $value
+	 * @return bool
+	 */
+	protected static function isNumericValue($value)
+	{
+		return is_numeric($value);
 	}
 }
