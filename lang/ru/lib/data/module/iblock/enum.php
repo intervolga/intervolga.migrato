ce0aa99b3b75a1980a74627c8201ae33978a9b3f
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-09-05 16:54:53 +0300

    MIGRATO-110: исправлена формулировка ошибок сложных XML ID

--- lang/ru/lib/data/module/iblock/enum.php
+++ lang/ru/lib/data/module/iblock/enum.php
@@ -1,4 +1,4 @@
 <?
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_ENUM'] = 'Списки свойств';
-$MESS['INTERVOLGA_MIGRATO.INVALID_IBLOCK_PROPERTY_ENUM_XML_ID'] = 'XML ID в БД не должен содержать точек';
+$MESS['INTERVOLGA_MIGRATO.INVALID_IBLOCK_PROPERTY_ENUM_XML_ID'] = 'XML ID должен иметь вид: <Инфоблок>.<Свойство>.<Значение>';
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_PROPERTY_ENUM_NOT_PROPERTY'] = 'Не существует пользовательское свойство #XML_ID#';
--- lang/ru/lib/data/module/iblock/property.php
+++ lang/ru/lib/data/module/iblock/property.php
@@ -1,4 +1,4 @@
 <?
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_PROPERTY'] = 'Свойства';
-$MESS['INTERVOLGA_MIGRATO.INVALID_XML_ID'] = 'XML ID свойства в БД не должен содержать точек';
+$MESS['INTERVOLGA_MIGRATO.INVALID_XML_ID'] = 'XML ID должен иметь вид: <Инфоблок>.<Свойство>';
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_NOT_FOUND'] = 'Инфоблок не найден: #IBLOCK#';
