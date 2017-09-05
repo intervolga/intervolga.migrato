0297abdc91bbe07d4083ec658b5c36e41d81fdc0
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-09-05 17:13:53 +0300

    MIGRATO-110: исправлена формулировка ошибки при импорте

--- lang/ru/lib/tool/console/command/importdatacommand.php
+++ lang/ru/lib/tool/console/command/importdatacommand.php
@@ -2,8 +2,8 @@
 $MESS['INTERVOLGA_MIGRATO.IMPORT_DATA_DESCRIPTION'] = 'Импорт данных из XML в БД';
 $MESS['INTERVOLGA_MIGRATO.STEP_ITERATE_IMPORT'] = 'Импорт данных с зависимостями';
 $MESS['INTERVOLGA_MIGRATO.RESOLVE'] = 'обновление ссылок';
-$MESS['INTERVOLGA_MIGRATO.NOT_RESOLVED'] = 'Ссылка не была обновлена';
-$MESS['INTERVOLGA_MIGRATO.NOT_RESOLVED_STEP'] = 'Проверка пропущенных ссылок';
+$MESS['INTERVOLGA_MIGRATO.NOT_RESOLVED'] = 'Запись для разрешения зависимости не найдена';
+$MESS['INTERVOLGA_MIGRATO.NOT_RESOLVED_STEP'] = 'Проверка пропущенных зависимостей';
 $MESS['INTERVOLGA_MIGRATO.STEP_SHOW_NOT_IMPORTED'] = 'Анализ пропущенных записей';
 $MESS['INTERVOLGA_MIGRATO.NEED_MORE_STEPS'] = 'Ошибка, требуется больше шагов импорта';
 $MESS['INTERVOLGA_MIGRATO.STEP_DELETE_MARKED'] = 'Удаление устаревших записей';
