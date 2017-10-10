8af7f9b72c45008d15032db8426da9b1527ebfdb
sarkisova elena <sarkisova@intervolga.ru>
2017-10-10 14:00:49 +0300

    MIGRATO-104: Изменены названия языковых переменных.

--- lang/ru/lib/data/module/iblock/filter.php
+++ lang/ru/lib/data/module/iblock/filter.php
@@ -1,3 +1,3 @@
 <?
-$MESS['INTERVOLGA_MIGRATO.IBLOCK_PROPERTY_FILTER_ADD_ERROR'] = 'Ошибка при создании фильтра';
-$MESS['INTERVOLGA_MIGRATO.IBLOCK_PROPERTY_FILTER_DELETE_ERROR'] = 'Ошибка при удалении фильтра';
+$MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_ADD_ERROR'] = 'Ошибка при создании фильтра';
+$MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_DELETE_ERROR'] = 'Ошибка при удалении фильтра';
