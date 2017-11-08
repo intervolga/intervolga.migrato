6083b5bf36b6d163fba735b76029e6121dbc0dd4
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2017-11-08 10:36:17 +0300

    MIGRATO-93: Мелкий рефакторинг

--- lib/data/module/catalog/iblock.php
+++ lib/data/module/catalog/iblock.php
@@ -7,12 +7,11 @@ use Bitrix\Catalog\CatalogIblockTable;
 use Intervolga\Migrato\Data\BaseData;
 use Intervolga\Migrato\Data\Link;
 use Intervolga\Migrato\Data\Module;
+use Intervolga\Migrato\Data\Module\Iblock\Iblock as IblockIblock;
 use Intervolga\Migrato\Data\Module\Iblock\Property;
 use Intervolga\Migrato\Data\Record;
 use Intervolga\Migrato\Data\RecordId;
 use Intervolga\Migrato\Tool\ExceptionText;
-use Intervolga\Migrato\Tool\XmlIdProvider\OrmXmlIdProvider;
-use Intervolga\Migrato\Tool\XmlIdProvider\BaseXmlIdProvider;
 
 Loc::loadMessages(__FILE__);
 
@@ -22,14 +21,13 @@ Loc::loadMessages(__FILE__);
  */
 class Iblock extends BaseData
 {
-
 	protected function configure()
 	{
-		Loader::includeModule("catalog");
+		Loader::includeModule('catalog');
 		$this->setEntityNameLoc(Loc::getMessage('INTERVOLGA_MIGRATO.CATALOG_IBLOCK'));
 		$this->setDependencies(array(
-			'IBLOCK_ID' => new Link(Module\Iblock\Iblock::getInstance()),
-			'PRODUCT_IBLOCK_ID' => new Link(Module\Iblock\Iblock::getInstance()),
+			'IBLOCK_ID' => new Link(IblockIblock::getInstance()),
+			'PRODUCT_IBLOCK_ID' => new Link(IblockIblock::getInstance()),
 			'SKU_PROPERTY_ID' => new Link(Property::getInstance()),
 		));
 		$this->setVirtualXmlId(true);
@@ -49,29 +47,35 @@ class Iblock extends BaseData
 			$id = $this->createId($iblock['IBLOCK_ID']);
 			$record->setId($id);
 			$record->setXmlId($this->getXmlId($id));
-			$dependency = clone $this->getDependency("IBLOCK_ID");
+			$dependency = clone $this->getDependency('IBLOCK_ID');
 			$dependency->setValue(
-				Module\Iblock\Iblock::getInstance()->getXmlId(RecordId::createStringId($iblock["IBLOCK_ID"]))
+				IblockIblock::getInstance()->getXmlId(RecordId::createStringId($iblock['IBLOCK_ID']))
 			);
-			$record->setDependency("IBLOCK_ID", $dependency);
+			$record->setDependency('IBLOCK_ID', $dependency);
 
-			if($iblock["PRODUCT_IBLOCK_ID"] > 0) {
-				$dependency = clone $this->getDependency("PRODUCT_IBLOCK_ID");
+			if ($iblock['PRODUCT_IBLOCK_ID'] > 0)
+			{
+				$dependency = clone $this->getDependency('PRODUCT_IBLOCK_ID');
 				$dependency->setValue(
-					Module\Iblock\Iblock::getInstance()->getXmlId(RecordId::createStringId($iblock["PRODUCT_IBLOCK_ID"]))
+					IblockIblock::getInstance()->getXmlId(RecordId::createStringId($iblock['PRODUCT_IBLOCK_ID']))
 				);
-				$record->setDependency("PRODUCT_IBLOCK_ID", $dependency);
-			} else {
+				$record->setDependency('PRODUCT_IBLOCK_ID', $dependency);
+			}
+			else
+			{
 				$record->addFieldsRaw(array('PRODUCT_IBLOCK_ID' => $iblock['PRODUCT_IBLOCK_ID']));
 			}
 
-			if($iblock["SKU_PROPERTY_ID"] > 0) {
-				$dependency = clone $this->getDependency("SKU_PROPERTY_ID");
+			if ($iblock['SKU_PROPERTY_ID'] > 0)
+			{
+				$dependency = clone $this->getDependency('SKU_PROPERTY_ID');
 				$dependency->setValue(
-					Property::getInstance()->getXmlId(RecordId::createStringId($iblock["SKU_PROPERTY_ID"]))
+					Property::getInstance()->getXmlId(RecordId::createStringId($iblock['SKU_PROPERTY_ID']))
 				);
-				$record->setDependency("SKU_PROPERTY_ID", $dependency);
-			} else {
+				$record->setDependency('SKU_PROPERTY_ID', $dependency);
+			}
+			else
+			{
 				$record->addFieldsRaw(array('SKU_PROPERTY_ID' => $iblock['SKU_PROPERTY_ID']));
 			}
 			$record->addFieldsRaw(array(
@@ -89,7 +93,7 @@ class Iblock extends BaseData
 	 */
 	public function getXmlId($id)
 	{
-		return Module\Iblock\Iblock::getInstance()->getXmlId($id);
+		return IblockIblock::getInstance()->getXmlId($id);
 	}
 
 	/**
@@ -101,10 +105,13 @@ class Iblock extends BaseData
 	{
 		$fields = $this->recordToArray($record);
 		$result = CatalogIblockTable::add($fields);
-		if($result->isSuccess()) {
+		if ($result->isSuccess())
+		{
 			$id = $this->createId($result->getData()['IBLOCK_ID']);
 			return $id;
-		} else {
+		}
+		else
+		{
 			throw new \Exception(ExceptionText::getFromApplication());
 		}
 	}
@@ -116,7 +123,8 @@ class Iblock extends BaseData
 	protected function deleteInner(RecordId $id)
 	{
 		$result = CatalogIblockTable::delete($id->getValue());
-		if(!$result->isSuccess()) {
+		if (!$result->isSuccess())
+		{
 			throw new \Exception(ExceptionText::getFromApplication());
 		}
 	}
@@ -129,7 +137,8 @@ class Iblock extends BaseData
 	{
 		$fields = $this->recordToArray($record);
 		$result = CatalogIblockTable::update($record->getId()->getValue(), $fields);
-		if(!$result->isSuccess()) {
+		if (!$result->isSuccess())
+		{
 			throw new \Exception(ExceptionText::getFromApplication());
 		}
 	}
@@ -140,21 +149,36 @@ class Iblock extends BaseData
 	 */
 	protected function recordToArray(Record $record)
 	{
-		$array = $record->getFieldsRaw(array("YANDEX_EXPORT", "SUBSCRIPTION"));
+		$array = $record->getFieldsRaw(array('YANDEX_EXPORT', 'SUBSCRIPTION'));
 
-		if($record->getDependency('IBLOCK_ID')->getValue() && $iblockId = $record->getDependency('IBLOCK_ID')->findId()) {
+		if ($record->getDependency('IBLOCK_ID')->getValue()
+			&& $iblockId = $record->getDependency('IBLOCK_ID')->findId()
+		)
+		{
 			$array['IBLOCK_ID'] = $iblockId->getValue();
 		}
 
-		if($record->getFieldRaw('PRODUCT_IBLOCK_ID') != '0' &&  $record->getDependency('PRODUCT_IBLOCK_ID')->getValue() && $iblockId = $record->getDependency('PRODUCT_IBLOCK_ID')->findId()) {
+		if ($record->getFieldRaw('PRODUCT_IBLOCK_ID') != '0'
+			&& $record->getDependency('PRODUCT_IBLOCK_ID')->getValue()
+			&& $iblockId = $record->getDependency('PRODUCT_IBLOCK_ID')->findId()
+		)
+		{
 			$array['PRODUCT_IBLOCK_ID'] = $iblockId->getValue();
-		} else {
+		}
+		else
+		{
 			$array['PRODUCT_IBLOCK_ID'] = $record->getFieldRaw('PRODUCT_IBLOCK_ID');
 		}
 
-		if($record->getFieldRaw('SKU_PROPERTY_ID') != '0' && $record->getDependency('SKU_PROPERTY_ID')->getValue() && $propertyId = $record->getDependency('SKU_PROPERTY_ID')->findId()) {
+		if ($record->getFieldRaw('SKU_PROPERTY_ID') != '0'
+			&& $record->getDependency('SKU_PROPERTY_ID')->getValue()
+			&& $propertyId = $record->getDependency('SKU_PROPERTY_ID')->findId()
+		)
+		{
 			$array['SKU_PROPERTY_ID'] = $propertyId->getValue();
-		} else {
+		}
+		else
+		{
 			$array['SKU_PROPERTY_ID'] = $record->getFieldRaw('SKU_PROPERTY_ID');
 		}
 
