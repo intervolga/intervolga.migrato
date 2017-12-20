7767c35bccb333f38a2cf2590e4a2a14f8aaa074
sarkisova elena <sarkisova@intervolga.ru>
2017-12-20 16:13:18 +0300

    MIGRATO-104: Для фильтра (просмотр разделов) добавлен языковой файл.

---
+++ lang/ru/lib/data/module/iblock/sectionfilter.php
@@ -0,0 +1,4 @@
+<?
+$MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_ADD_ERROR'] = 'Ошибка при создании фильтра';
+$MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_DELETE_ERROR'] = 'Ошибка при удалении фильтра';
+$MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_UPDATE_ERROR'] = 'Ошибка при обновлении фильтра';
