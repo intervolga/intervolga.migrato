9956fc47ec4c1b5fa1708b7a3217703d4ff6f5d3
Dmitry Akentyev <akentyev@intervolga.ru>
2018-06-21 15:37:44 +0300

    [MIGRATO-136] Начали импортировать в xml

--- lib/data/module/bizproc/workflowtemplate.php
+++ lib/data/module/bizproc/workflowtemplate.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Intervolga\Migrato\Data\Module\BizProc;
 
 use Bitrix\Main\Loader;
@@ -8,66 +9,75 @@ use Intervolga\Migrato\Data\Link;
 use Intervolga\Migrato\Data\Module\Iblock\Iblock as IblockIblock;
 use Intervolga\Migrato\Data\Record;
 use Intervolga\Migrato\Data\RecordId;
-use Intervolga\Migrato\Tool\Console\Logger;
+use Intervolga\Migrato\Tool\XmlIdProvider\BaseXmlIdProvider;
 
 Loc::loadMessages(__FILE__);
 
 class WorkflowTemplate extends BaseData
 {
 
-    protected function configure()
-    {
-        Loader::includeModule('bizproc');
-        $this->setEntityNameLoc(Loc::getMessage('INTERVOLGA_MIGRATO.BIZPROC_WORKFLOWTEMPLATE'));
-        //$this->setDependencies(array(
-        //    'IBLOCK_ID' => new Link(IblockIblock::getInstance()),
-        //));
-        $this->setVirtualXmlId(true);
-    }
+	protected function configure()
+	{
+		Loader::includeModule('bizproc');
+		$this->setEntityNameLoc(Loc::getMessage('INTERVOLGA_MIGRATO.BIZPROC_WORKFLOWTEMPLATE'));
+		$this->setDependencies(array(
+			'IBLOCK_ID' => new Link(IblockIblock::getInstance()),
+		));
+		$this->setVirtualXmlId(true);
+	}
 
-    /**
-     * @param array $filter
-     * @return array
-     */
-    public function getList(array $filter = array())
-    {
-        $result = array();
-        $dbTemplatesList = \CBPWorkflowTemplateLoader::GetList(Array(), Array());
-        while($arTemplate = $dbTemplatesList->Fetch()) {
-            $record = new \Intervolga\Migrato\Data\Record($this);
-            $id = $this->createId($arTemplate["ID"]);
-            $record->setId($id);
-            $record->setXmlId("WT_" . $id);
-            //$record->setXmlId($this->getXmlId($id));
-            $record->addFieldsRaw(array(
-                "TEMPLATE" => serialize($arTemplate)
-            ));
-            $result[] = $record;
-        }
-        return $result;
-    }
+	/**
+	 * @param array $filter
+	 * @return array
+	 */
+	public function getList(array $filter = array())
+	{
+		$result = array();
+		$dbTemplatesList = \CBPWorkflowTemplateLoader::GetList(array(), array());
+		while ($arTemplate = $dbTemplatesList->Fetch()) {
+			$record = new \Intervolga\Migrato\Data\Record($this);
+			$id = $this->createId($arTemplate["ID"]);
+			$record->setXmlId($this->getXmlId($id));
+			$record->setId($id);
+			$record->addFieldsRaw(array(
+				"TEMPLATE" => serialize($arTemplate)
+			));
+			$result[] = $record;
+		}
+		return $result;
+	}
 
-    /**
-     * @param RecordId $id
-     * @return string
-     */
-    public function getXmlId($id)
-    {
-//        return IblockIblock::getInstance()->getXmlId($id);
-    }
+	/**
+	 * @param RecordId $id
+	 * @return string
+	 */
+	public function getXmlId($id)
+	{
+		$dbTemplatesList = \CBPWorkflowTemplateLoader::GetList(array(), array("ID" => $id->getValue()));
+		if ($arTemplate = $dbTemplatesList->Fetch()) {
+			$md5 = md5(serialize(array(
+				$arTemplate["MODULE_ID"],
+				$arTemplate["ENTITY"],
+				$arTemplate["NAME"],
+				$arTemplate["DESCRIPTION"],
+				$arTemplate["DOCUMENT_TYPE"][2],
+			)));
+		};
+		return BaseXmlIdProvider::formatXmlId($md5);
+	}
 
-    public function setXmlId($id, $xmlId)
-    {
+	public function setXmlId($id, $xmlId)
+	{
 //        \CFormField::Set(array('SID' => $xmlId), $id->getValue());
-    }
+	}
 
-    /**
-     * @param Record $record
-     * @return RecordId
-     * @throws \Exception
-     */
-    protected function createInner(Record $record)
-    {
+	/**
+	 * @param Record $record
+	 * @return RecordId
+	 * @throws \Exception
+	 */
+	protected function createInner(Record $record)
+	{
 //        $fields = $this->recordToArray($record);
 //        $result = CatalogIblockTable::add($fields);
 //        if ($result->isSuccess())
@@ -79,33 +89,33 @@ class WorkflowTemplate extends BaseData
 //        {
 //            throw new \Exception(ExceptionText::getFromApplication());
 //        }
-    }
+	}
 
-    /**
-     * @param RecordId $id
-     * @throws \Exception
-     */
-    protected function deleteInner(RecordId $id)
-    {
+	/**
+	 * @param RecordId $id
+	 * @throws \Exception
+	 */
+	protected function deleteInner(RecordId $id)
+	{
 //        $result = CatalogIblockTable::delete($id->getValue());
 //        if (!$result->isSuccess())
 //        {
 //            throw new \Exception(ExceptionText::getFromApplication());
 //        }
-    }
+	}
 
-    /**
-     * @param Record $record
-     * @throws \Exception
-     */
-    public function update(Record $record)
-    {
+	/**
+	 * @param Record $record
+	 * @throws \Exception
+	 */
+	public function update(Record $record)
+	{
 //        $fields = $this->recordToArray($record);
 //        $result = CatalogIblockTable::update($record->getId()->getValue(), $fields);
 //        if (!$result->isSuccess())
 //        {
 //            throw new \Exception(ExceptionText::getFromApplication());
 //        }
-    }
+	}
 
 }
