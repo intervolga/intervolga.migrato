eb9075c840e3dc2f53853850d5baab61b1bdd31e
Arseny <ars@intervolga.ru>
2018-06-28 11:23:30 +0300

    [MIGRATO-136] Вынесли все ошибки в lang файлы.

--- lang/ru/lib/data/module/bizproc/workflowtemplate.php
+++ lang/ru/lib/data/module/bizproc/workflowtemplate.php
@@ -1,2 +1,6 @@
 <?php
 $MESS['INTERVOLGA_MIGRATO.BIZPROC_WORKFLOWTEMPLATE'] = 'Шаблоны Бизнес-процессов';
+$MESS['INTERVOLGA_MIGRATO.BIZPROC_WORKFLOWTEMPLATE.GXI.EX'] = 'Не могу получить шаблон-бизнес процесса с ID:';
+$MESS['INTERVOLGA_MIGRATO.BIZPROC_WORKFLOWTEMPLATE.CPN.EX'] = 'Некорректные символы в  XML_ID групп пользователей:';
+$MESS['INTERVOLGA_MIGRATO.BIZPROC_WORKFLOWTEMPLATE.CPN.EX2'] = 'Некорректные символы в  XML_ID групп пользователей:';
+$MESS['INTERVOLGA_MIGRATO.BIZPROC_WORKFLOWTEMPLATE.CPTN.EX'] = 'Некорректные символы в  XML_ID инфоблока:';
