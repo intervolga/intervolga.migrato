8d9b16f0e2223edd850d4b60a6c507db5fca6b4a
Alexander Petrenko <gafk555@gmail.com>
2018-07-17 15:55:35 +0300

    [MIGRATO-104]  Исправлена языковая переменная

--- lang/ru/lib/data/module/iblock/sectionfilter.php
+++ lang/ru/lib/data/module/iblock/sectionfilter.php
@@ -2,4 +2,4 @@
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_ADD_ERROR'] = 'Ошибка при создании фильтра';
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_DELETE_ERROR'] = 'Ошибка при удалении фильтра';
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_UPDATE_ERROR'] = 'Ошибка при обновлении фильтра';
-$MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_ENTITY_NAME'] = 'Фильтры разделов в административной части';
+$MESS['INTERVOLGA_MIGRATO.IBLOCK_FILTER_ENTITY_NAME'] = 'Фильтры разделов ИБ';
