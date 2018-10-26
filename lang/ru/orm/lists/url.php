f2e6e5aa85de4ed7e615fcaa1eb8d866c1516736
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2018-10-26 12:06:53 +0300

    [MIGRATO-146] Рефакторинг кода класса \Intervolga\Migrato\Orm\Lists\UrlTable

--- lang/ru/orm/lists/url.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS["URL_ENTITY_IBLOCK_ID_FIELD"] = "ID инфоблока";
-$MESS["URL_ENTITY_URL_FIELD"] = "—сылка в CRM";
-$MESS["URL_ENTITY_LIVE_FEED_FIELD"] = "ќтображать ли в живой ленте";
-?>
--- lib/orm/lists/url.php
+++ lib/orm/lists/url.php
@@ -1,74 +1,34 @@
 <?
 namespace Intervolga\Migrato\Orm\Lists;
 
-use Bitrix\Main,
-	Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Entity\DataManager;
+use Bitrix\Main\Entity\ReferenceField;
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Entity\IntegerField;
+use Bitrix\Main\Entity\StringField;
 
 Loc::loadMessages(__FILE__);
 
-/**
- * Class UrlTable
- *
- * Fields:
- * <ul>
- * <li> IBLOCK_ID int mandatory
- * <li> URL string(500) optional
- * <li> LIVE_FEED int optional
- * <li> IBLOCK reference to {@link \Bitrix\Iblock\IblockTable}
- * </ul>
- *
- * @package Intervolga\Migrato\Orm\Lists
- **/
-class UrlTable extends Main\Entity\DataManager
+class UrlTable extends DataManager
 {
-	/**
-	 * Returns DB table name for entity.
-	 *
-	 * @return string
-	 */
 	public static function getTableName()
 	{
 		return 'b_lists_url';
 	}
 
-	/**
-	 * Returns entity map definition.
-	 *
-	 * @return array
-	 */
 	public static function getMap()
 	{
 		return array(
-			'IBLOCK_ID' => array(
-				'data_type' => 'integer',
+			new IntegerField('IBLOCK_ID', array(
 				'primary' => true,
-				'title' => Loc::getMessage('URL_ENTITY_IBLOCK_ID_FIELD'),
+			)),
+			new StringField('URL'),
+			new IntegerField('LIVE_FEED'),
+			new ReferenceField(
+				'IBLOCK',
+				'\Bitrix\Iblock\Iblock',
+				array('=this.IBLOCK_ID' => 'ref.ID')
 			),
-			'URL' => array(
-				'data_type' => 'string',
-				'validation' => array(__CLASS__, 'validateUrl'),
-				'title' => Loc::getMessage('URL_ENTITY_URL_FIELD'),
-			),
-			'LIVE_FEED' => array(
-				'data_type' => 'integer',
-				'title' => Loc::getMessage('URL_ENTITY_LIVE_FEED_FIELD'),
-			),
-			'IBLOCK' => array(
-				'data_type' => 'Bitrix\Iblock\Iblock',
-				'reference' => array('=this.IBLOCK_ID' => 'ref.ID'),
-			),
-		);
-	}
-
-	/**
-	 * Returns validators for URL field.
-	 *
-	 * @return array
-	 */
-	public static function validateUrl()
-	{
-		return array(
-			new Main\Entity\Validator\Length(null, 500),
 		);
 	}
 }
