ef0b45f39d4bc37cf549bc1040f9537a599919b6
Vladislav Guriev <vladislav@intervolga.ru>
2018-12-13 09:17:11 +0400

    [MIGRATO-145] наработки для  прав на блоги

--- lib/data/module/blog/permission.php
+++ lib/data/module/blog/permission.php
@@ -17,7 +17,6 @@ class Permission extends BaseData
 	protected function configure()
 	{
 		$this->setVirtualXmlId(true);
-		Loader::includeModule("blog");
 		$this->setEntityNameLoc(Loc::getMessage('INTERVOLGA_MIGRATO.BLOG_PERMISSION'));
 		$this->setDependencies(array(
 			'GROUP_ID' => new Link(Group::getInstance()),
@@ -36,6 +35,7 @@ class Permission extends BaseData
 				"BLOG_ID" => $permission['BLOG_ID'],
 				"GROUP_ID" => $permission['USER_GROUP_ID'],
 			));
+
 			$record = new Record($this);
 			$record->setXmlId($this->getXmlId($id));
 			$record->setId($id);
@@ -54,31 +54,74 @@ class Permission extends BaseData
 
 			$result[] = $record;
 		}
-	}
-
-
-	/**
-	 * @return array|int[]
-	 */
-	protected function getBlogs()
-	{
-		$result = array();
-		$getList = \CBlog::GetList();
-		while ($blog = $getList->fetch())
-		{
-			$result[] = $blog["ID"];
-		}
 
 		return $result;
 	}
 
+//	public function update(Record $record)
+//	{
+//		$curValue = $record->getId()->getValue();
+//		$arGroups = \CBlogUserGroupPerms::GetGroupPermissions($curValue["BLOG_ID"]);
+//
+//		$curFields = $record->getFieldsRaw();
+//		$arGroups[$curValue["GROUP_ID"]] = $curFields["PERMISSION"];
+//		$blog = new \CBlog();
+//		$blog->SetPermission($curValue["BLOG_ID"], $arGroups);
+//	}
+
+//	protected function createInner(Record $record)
+//	{
+//		$blogLinkId = Blog::getInstance()->findRecord($record->getDependency("BLOG_ID")->getValue());
+//		$groupLinkId = Group::getInstance()->findRecord($record->getDependency("GROUP_ID")->getValue());
+//		if ($blogLinkId)
+//		{
+//			$blogId = $blogLinkId->getValue();
+//			$arGroups = \CBlogUserGroupPerms::getList($blogId);
+//			if ($groupLinkId)
+//			{
+//				$groupId = $groupLinkId->getValue();
+//				$arGroups[$groupId] = $record->getField("PERMISSION")->getValue();
+//				$iblock = new \CBlog();
+//				$iblock->setPermission($blogId, $arGroups);
+//
+//				return $this->createId(array(
+//					"BLOG_ID" => $blogId,
+//					"GROUP_ID" => $groupId,
+//				));
+//			}
+//			else
+//			{
+//				throw new \Exception(Loc::getMessage('INTERVOLGA_MIGRATO.GROUP_NOT_FOUND'));
+//			}
+//		}
+//		else
+//		{
+//			throw new \Exception(Loc::getMessage('INTERVOLGA_MIGRATO.IBLOCK_NOT_FOUND'));
+//		}
+//	}
+
+//	protected function deleteInner(RecordId $id)
+//	{
+//		$complexId = $id->getValue();
+//		$arGroups = \CBlogUserGroupPerms::getList($complexId["BLOG_ID"]);
+//		if (in_array($complexId['GROUP_ID'], array_keys($arGroups)))
+//		{
+//			unset($arGroups[$complexId['GROUP_ID']]);
+//			$iblock = new \CIBlock();
+//			$iblock->setPermission($complexId["BLOG_ID"], $arGroups);
+//		}
+//	}
 
 	public function getXmlId($id)
 	{
 		$array = $id->getValue();
 		$blogData = Blog::getInstance();
 		$groupData = Group::getInstance();
-		$blogXmlId = $blogData->getXmlId($blogData->createId($array['IBLOCK_ID']));
+
+
+
+
+		$blogXmlId = $blogData->getXmlId($blogData->createId($array['BLOG_ID']));
 		$groupXmlId = $groupData->getXmlId($groupData->createId($array['GROUP_ID']));
 		$md5 = md5(serialize(array(
 			$blogXmlId,
@@ -95,7 +138,6 @@ class Permission extends BaseData
 			)
 		);
 	}
-
 
 
 }
