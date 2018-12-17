f6b3e56cccd0a9dbc343722656fd07f6c782a989
Alexander Petrenko <gafk555@gmail.com>
2018-12-17 18:03:50 +0400

    [MIGRATO-104_options]: экспорт

--- lib/data/module/iblock/adminlistoption.php
+++ lib/data/module/iblock/adminlistoption.php
@@ -1,14 +1,17 @@
 <?php
 namespace Intervolga\Migrato\Data\Module\Iblock;
 
+use CUserOptions;
 use Bitrix\Main\Loader;
 use Bitrix\Main\Localization\Loc;
 use Intervolga\Migrato\Data\BaseData;
 use Intervolga\Migrato\Data\Link;
-use Intervolga\Migrato\Data\Module\Iblock\Iblock as MigratoIblock;
+use Intervolga\Migrato\Data\Record;
 
 /**
  * Class AdminListOptions - настройки отображения админ. страниц списка элементов и разделов инфоблока.
+ *
+ * Хэш названия настройки (<hash>): строка в которой закодирована информация об ИБ.
  *
  * @package Intervolga\Migrato\Data\Module\Iblock
  */
@@ -26,19 +29,19 @@ class AdminListOption extends BaseData
 	 * Префиксы названия настроек. После префиксов в названиях настроек идет <hash>:
 	 * option_name=<prefix><hash>
 	 *
-	 * IB_MIXED_LIST - список элементов и разделов ИБ 				( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
-	 * PRODUCTS_MIXED_LIST - список товаров и разделов каталога 	( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
+	 * IB_MIXED_LIST - список элементов и разделов ИБ              ( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
+	 * PRODUCTS_MIXED_LIST - список товаров и разделов каталога    ( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
 	 *
-	 * IB_SECTION_LIST - список разделов ИБ 						( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
-	 * PRODUCTS_SECTION_LIST - список разделов каталога 			( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
+	 * IB_SECTION_LIST - список разделов ИБ                        ( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
+	 * PRODUCTS_SECTION_LIST - список разделов каталога            ( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
 	 *
-	 * IB_ELEMENT_LIST - список элементов ИБ 						( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
-	 * PRODUCTS_LIST - список товаров 								( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
+	 * IB_ELEMENT_LIST - список элементов ИБ                       ( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
+	 * PRODUCTS_LIST - список товаров                              ( <hash>=md5(IBLOCK_TYPE_CODE.IBLOCK_ID) )
 	 *
-	 * IB_LIST - список ИБ конкретного типа 						( <hash> = md5(IBLOCK_TYPE_CODE) )
-	 * IB_LIST_ADMIN - список ИБ конкретного типа (admin=Y) 		( <hash> = md5(IBLOCK_TYPE_CODE) )
+	 * IB_LIST - список ИБ конкретного типа                        ( <hash> = md5(IBLOCK_TYPE_CODE) )
+	 * IB_LIST_ADMIN - список ИБ конкретного типа (admin=Y)        ( <hash> = md5(IBLOCK_TYPE_CODE) )
 	 *
-	 * IB_PROPERTIES_LIST - список свойств ИБ 						( <hash>=IBLOCK_ID )
+	 * IB_PROPERTIES_LIST - список свойств ИБ                      ( <hash>=IBLOCK_ID )
 	 */
 	const OPTION_NAME_PREFIXES = array(
 		'IB_MIXED_LIST' => 'tbl_iblock_list_',
@@ -56,6 +59,36 @@ class AdminListOption extends BaseData
 		'IB_PROPERTIES_LIST' => 'tbl_iblock_property_admin_',
 	);
 
+	/**
+	 * REGEX: имя настройки
+	 */
+	const OPTION_NAME_REGEX = '/^([a-z_]+_)(.+)$/';
+
+	/**
+	 * REGEX: название свойства ИБ
+	 */
+	const IBLOCK_PROPERTY_NAME_REGEX = '/^PROPERTY_(.+)$/';
+
+	/**
+	 * REGEX: название UF-поля
+	 */
+	const UF_FIELD_NAME_REGEX = '/^(UF_[A-Z0-9_]+)$/';
+
+	/**
+	 * Символ-разделитель логических блоков в строке с xmlId.
+	 */
+	const XML_ID_SEPARATOR = '.';
+
+	/**
+	 * Префикс свойства ИБ.
+	 */
+	const PROPERTY_FIELD_PREFIX = 'PROPERTY_';
+
+	/**
+	 * Префикс UF-поля.
+	 */
+	const UF_FIELD_PREFIX = 'UF_';
+
 	protected function configure()
 	{
 		Loader::includeModule('iblock');
@@ -63,7 +96,8 @@ class AdminListOption extends BaseData
 		$this->setVirtualXmlId(true);
 		$this->setFilesSubdir('/type/iblock/admin/');
 		$this->setDependencies(array(
-			'IBLOCK' => new Link(MigratoIblock::getInstance()),
+			'IBLOCK' => new Link(Iblock::getInstance()),
+			'IBLOCK_TYPE' => new Link(Type::getInstance()),
 			'PROPERTY' => new Link(Property::getInstance()),
 			'FIELD' => new Link(Field::getInstance()),
 		));
@@ -76,6 +110,465 @@ class AdminListOption extends BaseData
 	 */
 	public function getList(array $filter = array())
 	{
-		return parent::getList($filter);
+		$result = array();
+
+		/**
+		 * Типы мигрируемых фильтров:
+		 * - фильтры для админа
+		 * - общие фильтры
+		 */
+		$optionTypeFilters = array(
+			'ADMIN_OPTIONS' => array('USER_ID' => '1'),
+			'COMMON_OPTIONS' => array('COMMON' => 'Y', 'USER_ID' => '0'),
+		);
+
+		foreach ($optionTypeFilters as $optionTypeFilter)
+		{
+			$optionsFilter = array_merge($filter, $optionTypeFilter);
+			$dbRes = CUserOptions::GetList(array(), $optionsFilter);
+			while ($option = $dbRes->Fetch())
+			{
+				if (static::isOption($option))
+				{
+					$record = $this->createRecordFromArray($option);
+					$result[] = $record;
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param \Intervolga\Migrato\Data\RecordId $id
+	 *
+	 * @return string
+	 */
+	public function getXmlId($id)
+	{
+		$dbRes = CUserOptions::GetList(array(), array('ID' => $id->getValue()));
+		if ($filter = $dbRes->Fetch())
+		{
+			return $this->getXmlIdFromArray($filter);
+		}
+
+		return '';
+	}
+
+	protected function createRecordFromArray(array $option)
+	{
+		$record = new Record($this);
+		$record->setId($this->createId($option['ID']));
+		$record->setXmlId($this->getXmlIdFromArray($option));
+		$record->setFieldRaw('COMMON', $option['COMMON']);
+		$record->setFieldRaw('CATEGORY', $option['CATEGORY']);
+		$this->setIblockDependency($record, $option);
+		$this->setOptionValue($record, $option);
+
+		return $record;
+	}
+
+	protected function setOptionValue(Record $record, array $option)
+	{
+		$dependencies = array();
+
+		$arFields = $this->convertFieldsToXml($option, $dependencies);
+		$record->setFieldRaw('VALUE', serialize($arFields));
+
+
+		/**
+		 * Зависимости от сторонних сущностей:
+		 * - Свойства ИБ
+		 * - UF-поля
+		 */
+		foreach ($dependencies as $dependencyName => $dependencyXmlIds)
+		{
+			if ($dependencyXmlIds)
+			{
+				$dependencyXmlIds = array_unique($dependencyXmlIds);
+				$dependency = clone $this->getDependency($dependencyName);
+				$dependency->setValues($dependencyXmlIds);
+				$record->setDependency($dependencyName, $dependency);
+			}
+		}
+	}
+
+	protected function convertFieldsToXml(array $option, array &$dependencies)
+	{
+		$arOptionValue = unserialize($option['VALUE']);
+
+		foreach	($arOptionValue['views'] as $view)
+		{
+			$convertedProperties = array();
+
+			$viewColumns = $view['columns'];
+			$arViewColumns = explode(',', $viewColumns);
+			foreach ($arViewColumns as &$viewColumn)
+			{
+				if ($this->isIblockProperty($viewColumn))
+				{
+					$propertyName = $viewColumn;
+
+					$propertyXmlId = $this->convertIblockPropertyNameToXml($viewColumn);
+					$dependencies['PROPERTY'][] = $propertyXmlId;
+					$convertedProperties[$propertyName] = $viewColumn;
+
+				}
+				elseif($this->isUfField($viewColumn))
+				{
+					// TODO: Добавить в массив зависимостей
+				}
+			}
+			$viewColumns = implode(',', $arViewColumns);
+
+
+			// TODO: Конвертация названий свойств ИБ
+			$viewColumnSizes = $view['columns_sizes']['columns'];
+			$viewSortBy = $view['last_sort_by'];
+			$viewLastSortBy = $view['sort_by'];
+		}
+
+
+		return $arOptionValue;
+	}
+
+	protected function convertIblockPropertyNameToXml(&$iblockPropertyName)
+	{
+		$propertyXmlId = '';
+
+		$this->testStringAgainstIblockPropertyRegex($iblockPropertyName, $isMatch, $matches);
+		if ($isMatch && $matches[1])
+		{
+			$propertyId = intval($matches[1]);
+			$propertyIdObj = Property::getInstance()->createId($propertyId);
+			$propertyXmlId = Property::getInstance()->getXmlId($propertyIdObj);
+			$iblockPropertyName = static::PROPERTY_FIELD_PREFIX . $propertyXmlId;
+		}
+
+		return $propertyXmlId;
+	}
+
+	/**
+	 * Добавляет в запись миграции $record
+	 * зависимость от ИБ или типа ИБ.
+	 *
+	 * @param Record $record запись миграции настройки.
+	 * @param array $option массив данных настройки.
+	 */
+	protected function setIblockDependency(Record $record, array $option)
+	{
+		$dependencyXmlId = $this->getOptionIblockDependency($option['NAME']);
+		$dependencyName = $this->isOptionForIblock($option['NAME']) ? 'IBLOCK' : 'IBLOCK_TYPE';
+
+		if ($dependencyXmlId)
+		{
+			$dependency = clone $this->getDependency($dependencyName);
+			$dependency->setValue($dependencyXmlId);
+			$record->setDependency($dependencyName, $dependency);
+		}
+	}
+
+	/**
+	 * Возвращает xmlId настройки на основе массива ее данных $option.
+	 *
+	 * @param array $option массив данных настройки.
+	 *
+	 * @return string xmlId настройки.
+	 */
+	protected function getXmlIdFromArray(array $option)
+	{
+		$result = '';
+		$dependencyXmlId = $this->getOptionIblockDependency($option['NAME']);
+
+		if ($dependencyXmlId)
+		{
+			$filterType = $this->getOptionType($option['NAME']);
+
+			$result = (
+				$filterType
+				. static::XML_ID_SEPARATOR .
+				$dependencyXmlId
+				. static::XML_ID_SEPARATOR .
+				$option['USER_ID']
+			);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Возвращает xml_id основной зависимости настроки - зависимости от ИБ или типа ИБ.
+	 *
+	 * @param string $optionName название настройки.
+	 *
+	 * @return string xml_id основной зависимости настроки.
+	 */
+	protected function getOptionIblockDependency($optionName)
+	{
+		$dependencyXmlId = '';
+
+		$iblock = $this->getIblockByOptionName($optionName);
+		if ($iblock)
+		{
+			if ($this->isOptionForIblock($optionName))
+			{
+				$id = $iblock['ID'];
+				$className = Iblock::class;
+			}
+			else
+			{
+				$id = $iblock['IBLOCK_TYPE_ID'];
+				$className = Type::class;
+			}
+
+			/** @var BaseData $className */
+			$idObj = $className::getInstance()->createId($id);
+			$dependencyXmlId = $className::getInstance()->getXmlId($idObj);
+		}
+
+		return $dependencyXmlId;
+	}
+
+	/**
+	 * Возвращает ИБ по названию настройки.
+	 *
+	 * @param string $optionName название настройки.
+	 *
+	 * @return array данные ИБ.
+	 */
+	protected function getIblockByOptionName($optionName)
+	{
+		if (Loader::includeModule('iblock'))
+		{
+			$res = \CIBlock::GetList();
+			while ($iblock = $res->Fetch())
+			{
+				if ($this->isOptionIblock($iblock, $optionName))
+				{
+					return $iblock;
+				}
+			}
+		}
+
+		return array();
+	}
+
+	/**
+	 * Проверяет, принадлежит ли настройка $optionName инфоблоку $iblockInfo.
+	 * Проверка происходит по закодированному в названиии настройки <hash>.
+	 *
+	 * @param array $iblockInfo данные ИБ (ID и IBLOCK_TYPE_ID).
+	 * @param string $optionName название настройки.
+	 *
+	 * @return bool true, если настройка принадлежит ИБ, иначе - false.
+	 */
+	protected function isOptionIblock(array $iblockInfo, $optionName)
+	{
+		$iblockId = $iblockInfo['ID'];
+		$iblockTypeCode = $iblockInfo['IBLOCK_TYPE_ID'];
+		$hash = $this->getOptionHash($optionName);
+		$type = $this->getOptionType($optionName);
+
+		if ($hash && $type)
+		{
+			switch ($type)
+			{
+				case 'IB_MIXED_LIST':
+				case 'PRODUCTS_MIXED_LIST':
+				case 'IB_SECTION_LIST':
+				case 'PRODUCTS_SECTION_LIST':
+				case 'IB_ELEMENT_LIST':
+				case 'PRODUCTS_LIST':
+				{
+					return md5($iblockTypeCode . '.' . $iblockId) === $hash;
+				}
+				break;
+				case 'IB_LIST':
+				case 'IB_LIST_ADMIN':
+				{
+					return md5($iblockTypeCode) === $hash;
+				}
+				break;
+				case 'IB_PROPERTIES_LIST':
+				{
+					return $iblockId === $hash;
+				}
+				break;
+				default:
+				{
+					return false;
+				}
+				break;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Проверяет, привязана ли настройка к определенному ИБ
+	 * (если нет, то привязана к типу ИБ)
+	 *
+	 * @param string $optionName название настройки.
+	 *
+	 * @return bool true, если настройка привязана к ИБ, иначе - false.
+	 */
+	protected function isOptionForIblock($optionName)
+	{
+		$optionType = $this->getOptionType($optionName);
+
+		if ($optionType === 'IB_LIST_ADMIN' || $optionType === 'IB_LIST')
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	/**
+	 * Возвращает тип настройки.
+	 *
+	 * @param string $optionName название настройки (поле NAME настройки).
+	 *
+	 * @return string тип настройки.
+	 */
+	protected function getOptionType($optionName)
+	{
+		$isMatch = preg_match(static::OPTION_NAME_REGEX, $optionName, $matches);
+		if ($isMatch && $matches[1])
+		{
+			$prefix = $matches[1];
+			$key = array_search($prefix, static::OPTION_NAME_PREFIXES);
+
+			return $key !== false ? $key : '';
+		}
+
+		return '';
+	}
+
+	/**
+	 * Возвращает хэш названия настройки.
+	 *
+	 * @param string $optionName название настройки (поле NAME настройки).
+	 *
+	 * @return string хэш названия настройки.
+	 */
+	protected function getOptionHash($optionName)
+	{
+		$isMatch = preg_match(static::OPTION_NAME_REGEX, $optionName, $matches);
+		if ($isMatch && $matches[2])
+		{
+			return $matches[2];
+		}
+
+		return '';
+	}
+
+	/**
+	 * Проверяет, является ли $option настройкой отображения:
+	 *    - поле CATEGORY должно соответствовать одному из OPTION_CATEGORIES
+	 *    - префикс поля NAME должен соответствовать одному на OPTION_NAME_PREFIXES.
+	 *
+	 * @param array $option настройка.
+	 *
+	 * @return bool true, если $option - настройка отображения, иначе - false.
+	 */
+	protected static function isOption($option)
+	{
+		return static::isOptionCategory($option['CATEGORY'])
+			   && static::isOptionName($option['NAME']);
+	}
+
+	/**
+	 * Проверяет, является ли категория настройки $optionCategory
+	 * категорией настроек отображения.
+	 *
+	 * @param string $optionCategory категория настройки (поле CATEGORY настройки).
+	 *
+	 * @return bool true, если $optionCategory - категория настроек отображения, иначе - false.
+	 */
+	protected static function isOptionCategory($optionCategory)
+	{
+		return in_array($optionCategory, static::OPTION_CATEGORIES);
+	}
+
+	/**
+	 * Проверяет, является ли название настройки $optionName
+	 * названием настроек отображения.
+	 *
+	 * @param string $optionName название настройки (поле NAME настройки).
+	 *
+	 * @return bool true, если $optionName - название настроек отображения, иначе - false.
+	 */
+	protected static function isOptionName($optionName)
+	{
+		foreach (static::OPTION_NAME_PREFIXES as $optionNamePrefix)
+		{
+			if (strpos($optionName, $optionNamePrefix) === 0)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Проверяет, что строка $string является названием свойства ИБ.
+	 *
+	 * @param string $string проверяемая строка.
+	 *
+	 * @return bool true, если $string - название свойства ИБ, иначе - false.
+	 */
+	protected function isIblockProperty($string)
+	{
+		$isMatch = false;
+		$matches = array();
+
+		$this->testStringAgainstIblockPropertyRegex($string, $isMatch, $matches);
+
+		return ($isMatch && $matches[2]);
+	}
+
+	/**
+	 * Проверяет, что строка $string является названием UF-поля.
+	 *
+	 * @param string $string проверяемая строка.
+	 *
+	 * @return bool true, если $string - название UF-поля, иначе - false.
+	 */
+	protected function isUfField($string)
+	{
+		$length = strlen(static::UF_FIELD_PREFIX);
+
+		return (substr($string, 0, $length) === static::UF_FIELD_PREFIX);
+	}
+
+	/**
+	 * Проверяет строку $string на соответсвие регулярному выражению
+	 * для названия свойства ИБ.
+	 *
+	 * @param string $string проверяемая строка.
+	 * @param bool $isMatch признак соответствия проверяемой строки регулярному выражению.
+	 * @param array $matches массив совпадений.
+	 */
+	protected function testStringAgainstIblockPropertyRegex($string, &$isMatch, &$matches)
+	{
+		$isMatch = preg_match(static::IBLOCK_PROPERTY_NAME_REGEX, $string, $matches);
+	}
+
+	/**
+	 * Проверяет строку $string на соответсвие регулярному выражению
+	 * для названия UF-поля.
+	 *
+	 * @param string $string проверяемая строка.
+	 * @param bool $isMatch признак соответствия проверяемой строки регулярному выражению.
+	 * @param array $matches массив совпадений.
+	 */
+	protected function testStringAgainstUFRegex($string, &$isMatch, &$matches)
+	{
+		$isMatch = preg_match(static::UF_FIELD_NAME_REGEX, $string, $matches);
 	}
 }
