55a200999ebae045c7705e52f40a896eb6366392
IntervolgaVladislav <36883739+IntervolgaVladislav@users.noreply.github.com>
2019-07-15 13:29:51 +0400

    [MIGRATO-159] костяк для вывода диагностической информации

---
+++ lib/tool/console/command/diagnosticinformationcommand.php
@@ -0,0 +1,18 @@
+<?namespace Intervolga\Migrato\Tool\Console\Command;
+
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+class DiagnosticInformationCommand extends BaseCommand
+{
+	protected function configure()
+	{
+
+	}
+
+	public function executeInner()
+	{
+
+	}
+}
