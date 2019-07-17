10d46c6aa61d43d8c34f459137a4e81f059b9e8b
Vladislav Guriev <vladislav@intervolga.ru>
2019-07-17 13:26:35 +0400

    [MIGRATO-159] добавил начальный вывод необходимых данных в консоль

---
+++ lang/ru/lib/tool/console/command/diagnosticinformationcommand.php
@@ -0,0 +1,8 @@
+<?
+$MESS['INTERVOLGA_MIGRATO.DIAGNOSTIC_DESCRIPTION'] = 'Диагностическая информация';
+$MESS['INTERVOLGA_MIGRATO.DIAGNOSTIC_INFORMATION'] = '
+Версия модуля миграций: #VERSION_MIGRATION_MODULE#
+Версия главного модуля: #VERSION_MAIN_MODULE#
+Редакция Битрикса: #EDITORIAL_BITRIX#
+Версия PHP: #VERSION_PHP#
+';
--- lib/tool/console/command/diagnosticinformationcommand.php
+++ lib/tool/console/command/diagnosticinformationcommand.php
@@ -1,6 +1,7 @@
 <?namespace Intervolga\Migrato\Tool\Console\Command;
 
 use Bitrix\Main\Localization\Loc;
+use Intervolga\Migrato\Tool\Console\Logger;
 
 Loc::loadMessages(__FILE__);
 
@@ -8,11 +9,22 @@ class DiagnosticInformationCommand extends BaseCommand
 {
 	protected function configure()
 	{
-
+		$this->setName('diagnostic');
+		$this->setDescription(Loc::getMessage('INTERVOLGA_MIGRATO.DIAGNOSTIC_DESCRIPTION'));
 	}
 
 	public function executeInner()
 	{
-
+		$this->logger->registerFinal(Loc::getMessage(
+			'INTERVOLGA_MIGRATO.DIAGNOSTIC_INFORMATION',
+			array(
+				'#VERSION_MIGRATION_MODULE#' => '1',
+				'#VERSION_MAIN_MODULE#' => '2',
+				'#EDITORIAL_BITRIX#' => '3',
+				'#VERSION_PHP#' => '4',
+			)
+		),
+			Logger::TYPE_OK
+		);
 	}
 }
