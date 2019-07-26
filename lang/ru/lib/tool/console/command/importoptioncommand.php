b24a142ab27429645f24218d614316e6f5e914d8
Dmitry Matyushechkin <dm@intervolga.ru>
2019-07-26 10:30:47 +0400

    [MIGRATO-160] Исправлена языковая переменная INTERVOLGA_MIGRATO.IMPORT_OPTIONS_DESCRIPTION

--- lang/ru/lib/tool/console/command/importoptioncommand.php
+++ lang/ru/lib/tool/console/command/importoptioncommand.php
@@ -1,3 +1,3 @@
 <?
-$MESS['INTERVOLGA_MIGRATO.IMPORT_OPTIONS_DESCRIPTION'] = 'Импорт настроек модулей из БД в XML';
+$MESS['INTERVOLGA_MIGRATO.IMPORT_OPTIONS_DESCRIPTION'] = 'Импорт настроек модулей из XML в БД';
 $MESS['INTERVOLGA_MIGRATO.IMPORT_OPTION'] = 'импорт настройки';
