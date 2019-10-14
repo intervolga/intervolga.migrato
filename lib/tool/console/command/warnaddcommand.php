f36de9c41174f060852428f753e75212251f23ab
IntervolgaVladislav <36883739+IntervolgaVladislav@users.noreply.github.com>
2019-10-14 11:07:32 +0400

    [MIGRATO-154] рефактор, наработки по отображению создаваемых записей

--- lib/tool/console/command/warnaddcommand.php
+++ lib/tool/console/command/warnaddcommand.php
@@ -6,6 +6,7 @@ use Intervolga\Migrato\Data\Record;
 use Intervolga\Migrato\Tool\Config;
 use Intervolga\Migrato\Tool\Console\Logger;
 use Intervolga\Migrato\Tool\DataFileViewXml;
+use Intervolga\Migrato\Data\RecordId;
 
 Loc::loadMessages(__FILE__);
 
@@ -25,69 +26,12 @@ class WarnAddCommand extends BaseCommand
 
 	public function executeInner()
 	{
-		$this->willAdd = 0;
-		$addedRecords = array();
 		$configDataClasses = Config::getInstance()->getDataClasses();
 
 		foreach ($configDataClasses as $dataClass)
 		{
 			$this->checkData($dataClass);
 		}
-
-
-		// найди расходящиеся сущности, чтобы потом применить $this->logger->addDb
-		foreach ($this->filesInDirectories as $module => $entity)
-		{
-			foreach ($entity as $record)
-			{
-				//"\Intervolga\Migrato\Data\\" . $module->findRecord($record);
-			}
-		}
-
-
-
-
-//		foreach ($this->filesInDirectories as $dirFiles => $files)
-//		{
-//			foreach ($this->filesInDatabase as $dirDatabase => $records)
-//			{
-//				if ($dirFiles == $dirDatabase) {
-//					$result = array_diff($files, $records);
-//					$addedRecords[$dirFiles] = array_values($result);
-//					$this->willAdd += count($result);
-//				}
-//			}
-//		}
-
-
-//		foreach ($addedRecords as $dir => $records)
-//		{
-//			if(count($records) > 0)
-//			{
-//				$this->logger->add(
-//					Loc::getMessage(
-//						'INTERVOLGA_MIGRATO.RECORDS_WILL_BE_ADD',
-//						array(
-//							'#PATH#' => $dir,
-//							'#TOTAL#' => count($records),
-//						)
-//					),
-//					0,
-//					Logger::TYPE_INFO
-//				);
-//			}
-//
-////			foreach ($configDataClasses as $dataClass)
-////			{
-////				$path = INTERVOLGA_MIGRATO_DIRECTORY . $dataClass->getModule() . $dataClass->getFilesSubdir() . $dataClass->getEntityName() . '/';
-////
-////				if($path == $dir)
-////				{
-////					$this->checkAddedRecords($dataClass, $records);
-////				}
-////			}
-//
-//		}
 
 		$this->addResult();
 	}
@@ -97,71 +41,83 @@ class WarnAddCommand extends BaseCommand
 	 */
 	protected function checkData(BaseData $dataClass)
 	{
-		$this->getFileRecordsXmlIds($dataClass);
-		$path = INTERVOLGA_MIGRATO_DIRECTORY . $dataClass->getModule() . $dataClass->getFilesSubdir() . $dataClass->getEntityName() . '/';
+		$recordsInFiles = self::getRecordsInFiles($dataClass);
+		$recordsInDatabase = self::getRecordsInDatabase($dataClass);
 
-		$databaseRecords = $dataClass->getList();
-		foreach ($databaseRecords as $databaseRecord)
+		////////////////////////////////////////////////////
+		// для ТЕСТА
+		if($recordsInDatabase['main']['group'])
 		{
-			$this->totalRecords++;
-			$this->filesInDatabase[$dataClass->getModule()][$dataClass->getEntityName()][] = $databaseRecord->getXmlId();
+			$recordsInDatabase['main']['group'][] = 'test_entitity in database';
 		}
-	}
+		// для ТЕСТА
+		if($recordsInFiles['main']['group'])
+		{
+			$recordsInFiles['main']['group'][] = 'test_entitity in files 1';
+			$recordsInFiles['main']['group'][] = 'test_entitity in files 2';
+		}
+		if($recordsInFiles['main']['event'])
+		{
+			$recordsInFiles['main']['event'][] = 'test_event in files 1';
+		}
+		////////////////////////////////////////////////////
 
-	/**
-	 * @param \Intervolga\Migrato\Data\BaseData $dataClass
-	 * @param array $records
-	 */
-	protected function checkAddedRecords(BaseData $dataClass, $records)
-	{
-		$databaseRecords = $dataClass->getList();
-
-
-//		foreach ($databaseRecords as $databaseRecord)
-//		{
-//			if (!in_array($databaseRecord->getXmlId(), $records))
-//			{
-//				$this->logger->addDb(array(
-//					'RECORD' => $databaseRecord,
-//					'OPERATION' => Loc::getMessage('INTERVOLGA_MIGRATO.RECORDS_WILL_BE_ADD'),
-//				));
-//			}
-//		}
+		// вычислить расхождение между файлами и БД
+		$recordsWillAdd = array_diff($recordsInFiles[$dataClass->getModule()][$dataClass->getEntityName()], $recordsInDatabase[$dataClass->getModule()][$dataClass->getEntityName()]);
 
 
-//		foreach ($records as $records)
-//		{
-//
-//			$this->logger->addDb(array(
-//				//'RECORD' => $databaseRecord,
-//				'OPERATION' => Loc::getMessage('INTERVOLGA_MIGRATO.RECORD_WILL_BE_ADD'),
-//			));
-//
-//		}
-
+		foreach ($recordsWillAdd as $record)
+		{
+			$this->willAdd++;
+			$this->logger->addDb(array(
+				'MODULE_NAME' => $dataClass->getModule(),
+				'ENTITY_NAME' => $dataClass->getEntityName(),
+				//'XML_ID' => RecordId::createStringId($record),
+				'ID' => RecordId::createStringId($record),
+				'OPERATION' => Loc::getMessage('INTERVOLGA_MIGRATO.RECORD_WILL_BE_ADD'),
+			));
+		}
 	}
 
 	/**
 	 * @param \Intervolga\Migrato\Data\BaseData $dataClass
 	 *
-	 * @return string[]
+	 * return array
 	 */
-	protected function getFileRecordsXmlIds(BaseData $dataClass)
+	protected function getRecordsInFiles(BaseData $dataClass)
 	{
+		$records = array();
 		$path = INTERVOLGA_MIGRATO_DIRECTORY . $dataClass->getModule() . $dataClass->getFilesSubdir() . $dataClass->getEntityName() . '/';
 		$fileRecords = DataFileViewXml::readFromFileSystem($path);
-		$fileRecordsXmlIds = array();
 
 		foreach ($fileRecords as $record)
 		{
 			if (!$record->getDeleteMark())
 			{
-				$fileRecordsXmlIds[] = $record->getXmlId();
+				$this->totalRecords++;
+				$records[$dataClass->getModule()][$dataClass->getEntityName()][] = $record->getXmlId();
 			}
 		}
-		$this->filesInDirectories[$dataClass->getModule()][$dataClass->getEntityName()] = $fileRecordsXmlIds;
 
-		return $fileRecordsXmlIds;
+		return $records;
+	}
+
+	/**
+	 * @param \Intervolga\Migrato\Data\BaseData $dataClass
+	 *
+	 * return array
+	 */
+	protected function getRecordsInDatabase(BaseData $dataClass)
+	{
+		$records = array();
+		$databaseRecords = $dataClass->getList();
+
+		foreach ($databaseRecords as $databaseRecord)
+		{
+			$records[$dataClass->getModule()][$dataClass->getEntityName()][] = $databaseRecord->getXmlId();
+		}
+
+		return $records;
 	}
 
 	protected function addResult()
