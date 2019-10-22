902eed35a268d8da10cb3578f36a6fd08bef878e
IntervolgaVladislav <36883739+IntervolgaVladislav@users.noreply.github.com>
2019-10-22 15:26:02 +0400

    [MIGRATO-148] наработки по запросу

--- lib/tool/console/command/backup.php
+++ lib/tool/console/command/backup.php
@@ -2,7 +2,7 @@
 namespace Intervolga\Migrato\Tool\Console\Command;
 
 use Bitrix\Main\Localization\Loc;
-use Bitrix\Main\IO\File;
+use Bitrix\Main\Web\HttpClient;
 use Intervolga\Migrato\Data\Module;
 
 Loc::loadMessages(__FILE__);
@@ -14,8 +14,35 @@ class Backup extends BaseCommand
 		$this->setName('backup');
 		$this->setDescription(Loc::getMessage('INTERVOLGA_MIGRATO.BACKUP_DESCRIPTION'));
 	}
+
 	public function executeInner()
 	{
+		static::createBackup();
+	}
+
+	protected static function createBackup()
+	{
+		global $USER;
+		$USER->Authorize(1);
+
+		$httpClient = new HttpClient();
+		$httpClient->setAuthorization($USER->GetLogin(), ''); // login and password
+
+		//$sessid = explode("=", bitrix_sessid_get())[1];
+		$postData = array(
+			"lang" => 'ru',
+			"process" => 'Y',
+			"action" => 'start',
+			"dump_bucket_id" => 0, // Размещение резервной копии
+			"dump_all" => "Y",
+			"sessid" => ''
+		);
+
+
+		$response = $httpClient->post("http://gurjev.ivdev.ru/bitrix/admin/dump.php", $postData);
+
+
+
 
 	}
 }
