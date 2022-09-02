4948ca2e5874d3d83f4371b49dc9a2fce09385d9
ivsupport <info@intervolga.ru>
2022-09-02 14:05:03 +0300

    [MIGRATO-148] Работа над запуском создания бекапов

--- lang/ru/lib/tool/console/command/backup.php
+++ lang/ru/lib/tool/console/command/backup.php
@@ -1,2 +1,8 @@
 <?
 $MESS['INTERVOLGA_MIGRATO.BACKUP_DESCRIPTION'] = 'Создать резервную копию сайта';
+
+$MESS['INTERVOLGA_MIGRATO.BACKUP_ARGS_NOKERNEL'] = 'Не добавлять ядро сайта';
+$MESS['INTERVOLGA_MIGRATO.BACKUP_ARGS_NOPUBLIC'] = 'Не добавлять публичную часть';
+$MESS['INTERVOLGA_MIGRATO.BACKUP_ARGS_NODATABASE'] = 'Не добавлять базу данных';
+$MESS['INTERVOLGA_MIGRATO.BACKUP_ARGS_NOUPLOAD'] = 'Не добавлять папку upload';
+$MESS['INTERVOLGA_MIGRATO.BACKUP_ARGS_FILESIZE'] = 'Максимальный размер одного файла бекапа';
--- lib/tool/console/command/backup.php
+++ lib/tool/console/command/backup.php
@@ -4,59 +4,201 @@ namespace Intervolga\Migrato\Tool\Console\Command;
 use Bitrix\Main\Localization\Loc;
 use Bitrix\Main\Web\HttpClient;
 use Intervolga\Migrato\Data\Module;
+use Symfony\Component\Console\Input\InputOption;
 
 Loc::loadMessages(__FILE__);
 
 class Backup extends BaseCommand
 {
+	protected $httpClient;
+	protected $site;
+	protected $login = 'admin';
+	protected $password = '123456';
+	protected $exec_time = 120;
+	protected $exec_time_sleep = 5;
+	protected $cookiesFile = __DIR__.'/cookies.dat';
+
+	private function getCookiesInfo()
+	{
+		if (!is_file($this->cookiesFile))
+		{
+			return [];
+		}
+		$content = file_get_contents($this->cookiesFile);
+		$sessionInfo = json_decode($content, true);
+		return $sessionInfo;
+	}
+
+	private function setCookiesInfo($info)
+	{
+		file_put_contents($this->cookiesFile, json_encode($info));
+	}
+
+	private function delCookiesInfo()
+	{
+		if (is_file($this->cookiesFile))
+		{
+			unlink($this->cookiesFile);
+		}
+	}
+
 	protected function configure()
 	{
 		$this->setName('backup');
 		$this->setDescription(Loc::getMessage('INTERVOLGA_MIGRATO.BACKUP_DESCRIPTION'));
+		$this->addOption(
+			'nokernel',
+			null,
+			InputOption::VALUE_NONE,
+			Loc::getMessage("INTERVOLGA_MIGRATO.BACKUP_ARGS_NOKERNEL")
+		);
+		$this->addOption(
+			'nodatabase',
+			null,
+			InputOption::VALUE_NONE,
+			Loc::getMessage("INTERVOLGA_MIGRATO.BACKUP_ARGS_NODATABASE")
+		);
+		$this->addOption(
+			'nopublic',
+			null,
+			InputOption::VALUE_NONE,
+			Loc::getMessage("INTERVOLGA_MIGRATO.BACKUP_ARGS_NOPUBLIC")
+		);
+		$this->addOption(
+			'noupload',
+			null,
+			InputOption::VALUE_NONE,
+			Loc::getMessage("INTERVOLGA_MIGRATO.BACKUP_ARGS_NOUPLOAD")
+		);
+		$this->addArgument(
+			'size',
+			InputOption::VALUE_OPTIONAL,
+			Loc::getMessage("INTERVOLGA_MIGRATO.BACKUP_ARGS_FILESIZE")
+		);
 	}
 
 	public function executeInner()
 	{
-		static::createBackup();
+		$this->httpClient = new HttpClient();
+		$this->httpClient->setCookies($this->getCookiesInfo());
+		$this->site = $this->getSiteInfo();
+		// static::createBackup();
+
+
+
+		$html = $this->get('/local/modules/intervolga.migrato/tools/check_session.php');
+		$cookies = $this->httpClient->getCookies()->toArray();
+		con3([
+			'first',
+			'result'=>$html,
+			'cookies'=>$cookies,
+			'type of cookies' => gettype($cookies),
+		]);
+
+		$cookies2 = $this->httpClient->getCookies()->toArray();
+		con3([
+			'second',
+			'result'=>$html,
+			'cookies'=>$cookies,
+			'type of cookies' => gettype($cookies),
+		]);
 	}
 
-	protected static function createBackup()
+	protected function getSiteInfo()
 	{
-		global $USER;
-		$USER->Authorize(1);
-
-		$httpClient = new HttpClient();
-		//$httpClient->setHeader('Content-Type', 'application/json', true);
-		$httpClient->setAuthorization($USER->GetLogin(), '91sede'); // login and password
-
-
 		$site = array();
 		$defSite = \Bitrix\Main\SiteTable::getList(array('filter' => array('DEF' => 'Y')));
 		if  ($arSite = $defSite->fetch())
 		{
 			$site = $arSite;
+			if (!$site['SERVER_NAME'])
+			{
+				$site['SERVER_NAME'] = basename($_SERVER['DOCUMENT_ROOT']);
+			}
+		}
+		return $site;
+	}
+
+	protected function makeRequest($urlPath, $isPost=false, $postData=[])
+	{
+		$fullUrl = 'http://'.$this->site['SERVER_NAME'].$urlPath;
+		$this->httpClient->setHeader('Authorization', 'Basic '.base64_encode($this->login.':'.$this->password), true);
+		$cookies = $this->getCookiesInfo();
+		if (!$cookie)
+		{
+			// $this->httpClient->setCookies($cookies);
+		}
+		if ($isPost)
+		{
+			$html = $this->httpClient->post($fullUrl, $postData);
+		} else {
+			$html = $this->httpClient->get($fullUrl);
+		}
+		$cookies = $this->httpClient->getCookies()->toArray();
+		$this->setCookiesInfo($cookies);
+
+		return $html;
+	}
+
+	protected function get($urlPath)
+	{
+		return $this->makeRequest($urlPath);
+	}
+
+	protected function post($urlPath, $postData=[])
+	{
+		return $this->makeRequest($urlPath, true, $postData);
+	}
+
+	protected function prepareParams()
+	{
+		$params = $this->input->getArgument('size');
+		$size = count($params) ? $params[0] : 100;
+
+		$postData = array(
+			"lang" => "ru",
+			"process" => "Y",
+			"action" => "start",
+			"dump_bucket_id" => 0,
+			"dump_max_exec_time" => $this->exec_time,
+			"dump_max_exec_time_sleep" => $this->exec_time_sleep,
+			"dump_archive_size_limit" => $size,
+			"max_file_size" => 0,
+		);
+		if (!$this->input->getOption('nodatabase'))
+		{
+			$postData["dump_base"] = "Y";
+		}
+		if (!$this->input->getOption('nokernel'))
+		{
+			$postData["dump_file_kernel"] = "Y";
+		}
+		if (!$this->input->getOption('nopublic'))
+		{
+			$postData["dump_file_public"] = "Y";
+		}
+		if ($this->input->getOption('noupload'))
+		{
+			$postData["skip_mask"] = "Y";
+			$postData["arMask"] = ["/upload"];
 		}
 
-		$sessid = explode("=", bitrix_sessid_get())[1];
-		$postData = json_encode(array(
-			"lang" => 'ru',
-			"process" => 'Y',
-			"action" => 'start',
-			"dump_bucket_id" => 0, // Размещение резервной копии
-			//"dump_all" => "Y",
-			"dump_max_exec_time" => 20,
-			"dump_max_exec_time_sleep" => 1,
-			"dump_archive_size_limit" => 100,
-			"dump_integrity_check" => 'Y',
-			"dump_base" => 'Y',
-			"dump_base_skip_stat" => 'Y',
-			"dump_base_skip_search" => 'Y',
-			"dump_base_skip_log" => 'Y',
-			"sessid" => $sessid
-		));
+		return $postData;
+	}
 
-
-		$response = $httpClient->post("http://" . $site['SERVER_NAME'] . "/bitrix/admin/dump.php", $postData);
-
+	protected function createBackup()
+	{
+		$startUrl = "/bitrix/admin/dump.php";
+		// $postData = $this->prepareParams();
+		// $postData['sessid'] = bitrix_sessid();
+		// $response = $this->post($startUrl, $postData);
+		// con3($response);
+		//
+		// preg_match ('/AjaxSend\([\'\"]([^\'\"]+)[\'\"]\)/ui', $response, $parts);
+		// $nextStepUrl = $startUrl . $parts[1];
+		$nextStepUrl = $startUrl . '?process=Y&sessid=d2652ed05a704e8fabf66addd57eebb7';
+		con3($nextStepUrl);
+		$response = $this->get($nextStepUrl);
+		con3($response);
 	}
 }
