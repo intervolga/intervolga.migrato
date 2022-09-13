858837eb7b1bf457f43a2f1de2529b4dce9bf45c
Иван Жданов <zhdanov@intervolga.ru>
2022-09-13 12:38:22 +0300

    [MIGRATO-170] Lang-файлы

--- lang/ru/lib/tool/console/command/diffcommand.php
+++ lang/ru/lib/tool/console/command/diffcommand.php
@@ -1,5 +1,11 @@
 <?
 $MESS['INTERVOLGA_MIGRATO.DIFF_DESCRIPTION'] = 'Проверка XML => БД';
+$MESS['INTERVOLGA_MIGRATO.NO_AFTER_CLEAR_DESCRIPTION'] = 'Пропустить сброс кэша, поискового индекса и urlrewrite.php';
+$MESS['INTERVOLGA_MIGRATO.IGNORE_NEW_BACKUPS'] = 'Импортировать данные в базу без свежей резервной копии';
+$MESS['INTERVOLGA_MIGRATO.OPTION_SAFE_DELETE'] = 'Не удалять записи, которых нет в XML';
+$MESS['INTERVOLGA_MIGRATO.NO_ACTUAL_BACKUP'] = 'Нет полного бекапа сайта. Сделайте полный бекап или используйте параметр --force';
+$MESS['INTERVOLGA_MIGRATO.NOT_ACTUAL_BACKUP'] = 'Полный бекап был снят слишком давно: #LAST_DATE#. Сделайте полный бекап или используйте параметр --force';
+$MESS['INTERVOLGA_MIGRATO.FOUND_ACTUAL_BACKUP'] = 'Найден актуальный бекап сайта от #LAST_DATE#';
 $MESS['INTERVOLGA_MIGRATO.OPTION_SHOW_CHANGES'] = 'Показывать только изменения';
 $MESS['INTERVOLGA_MIGRATO.OPTION_SHOW_NOCHANGES'] = 'Показывать только то, что не изменилось';
 $MESS['INTERVOLGA_MIGRATO.OPTION_SHOW_BOTHCHANGES'] = 'Показывать всё';
--- lang/ru/lib/tool/console/command/diffdatacommand.php
+++ lang/ru/lib/tool/console/command/diffdatacommand.php
@@ -1,2 +1,5 @@
 <?
 $MESS['INTERVOLGA_MIGRATO.DIFF_DATA_DESCRIPTION'] = 'Проверка данных из XML и БД на соответствие';
+$MESS['INTERVOLGA_MIGRATO.DEPENDENCY_DESCRIPTION'] = '#NAME#: #VALUES#';
+$MESS['INTERVOLGA_MIGRATO.NEED_MORE_STEPS'] = 'Ошибка, требуется больше шагов импорта';
+$MESS['INTERVOLGA_MIGRATO.RECORD_NOT_FOUND'] = 'Запись не найдена';
