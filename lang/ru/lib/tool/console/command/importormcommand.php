27fc65be4eb1268e929c0ab847f903bce46e3f40
Mihail_E <me@intervolga.ru>
2025-05-19 17:49:43 +0500

    [MIGRATO-184] Добавил проверку на существование модуля.

--- lang/ru/lib/tool/console/command/importormcommand.php
+++ lang/ru/lib/tool/console/command/importormcommand.php
@@ -1,3 +1,4 @@
-<?
+<?php
 $MESS['INTERVOLGA_MIGRATO.IMPORT_ORM_DESCRIPTION'] = 'Актуализирует структуру таблиц в соответствии с их ORM-классами';
 $MESS['INTERVOLGA_MIGRATO.IMPORT_ORM'] = 'импорт ORM';
+$MESS['INTERVOLGA_MIGRATO.IMPORT_ORM_MODULE_NOT_FOUND'] = 'Модуль #MODULE_ID# не установлен.';
