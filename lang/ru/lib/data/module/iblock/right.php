9b79925856ffc5c2efab88a3e096fa8a0cf4f06a
Иван Трутнев <trutnev@intervolga.ru>
2026-03-13 10:16:10 +0300

    [MIGRATO-191] убрал пробел в конце lang записи

--- lang/ru/lib/data/module/iblock/right.php
+++ lang/ru/lib/data/module/iblock/right.php
@@ -1,6 +1,6 @@
 <?php
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_RIGHTS'] = 'Расширенные права доступа';
-$MESS['INTERVOLGA_MIGRATO.TASK_NOT_FOUND'] = 'Не найдено описание права доступа ';
+$MESS['INTERVOLGA_MIGRATO.TASK_NOT_FOUND'] = 'Не найдено описание права доступа';
 $MESS['INTERVOLGA_MIGRATO.IBLOCK_NOT_FOUND'] = 'Не найден инфоблок';
 $MESS['INTERVOLGA_MIGRATO.GROUP_NOT_FOUND'] = 'Не найдена группа пользователя';
 $MESS['INTERVOLGA_MIGRATO.INVALID_IBLOCK_ID'] = 'Неверный формат ID инфоблока. ID должен быть > 0';
